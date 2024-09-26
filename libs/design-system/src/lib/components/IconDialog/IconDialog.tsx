e67c0c7699e484b6014be4b0c0d3042be15d0b18
reza <rezasiahi8@gmail.com>
2024-09-26 11:21:27 +0330

    feat: add icon from Icon component

--- libs/design-system/src/lib/components/IconDialog/IconDialog.tsx
+++ libs/design-system/src/lib/components/IconDialog/IconDialog.tsx
@@ -1,6 +1,7 @@
 import { Button, Dialog, DialogPanel, Transition } from '@headlessui/react';
 import { cn } from '../../../utils';
 import { Fragment } from 'react';
+import { Icon } from '../IconComponent';
 
 // type data prop for modal
 interface IconDialogItem {
@@ -44,7 +45,7 @@ export function IconDialog({
           >
             <DialogPanel
               transition
-              className="w-96 h-60 relative flex flex-col items-center shadow rounded-[20px] pt-16 px-6 pb-6"
+              className="w-96 h-60 relative flex flex-col items-center shadow-lg rounded-3xl pt-16 px-6 pb-6"
             >
               <div
                 className={cn(
@@ -58,17 +59,14 @@ export function IconDialog({
                 >
                   <div
                     className={cn(
-                      `rounded-full px-4 w-16 flex justify-center items-center h-16 ${colors[mode][2]}`
+                      `rounded-full text-white px-4 w-16 flex justify-center items-center h-16 ${colors[mode][2]}`
                     )}
                   >
-                    <img
-                      src={`${
-                        mode === 'error'
-                          ? 'icons/vectors/error.svg'
-                          : 'icons/vectors/success.svg'
-                      }`}
-                      alt="vector icon"
-                    />
+                    {mode === 'error' ? (
+                      <Icon name="x" size="lg" />
+                    ) : (
+                      <Icon name="check" size="lg" />
+                    )}
                   </div>
                 </div>
               </div>
@@ -78,7 +76,7 @@ export function IconDialog({
               <p className="mt-4 text-center leading-[30px]">{message}</p>
               <Button
                 onClick={onClose}
-                className="w-full mt-6 py-2 text-white rounded-lg font-semibold transition-colors duration-300 h-12 hover:bg-brand-700 bg-[#009695]"
+                className="w-full mt-6 text-white py-2 rounded-lg font-semibold transition-colors duration-300 hover:bg-brand-700 bg-[#009695]"
               >
                 بستن
               </Button>
