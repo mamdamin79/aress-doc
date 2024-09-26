ca48d49228f7c6d5c16397278751250ae80b7528
reza <rezasiahi8@gmail.com>
2024-09-26 14:40:25 +0330

    feat: add font vazrimatn in design-system

--- libs/design-system/tailwind.config.js
+++ libs/design-system/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        vazirmant: 'vazirmatn',
+      },
       boxShadow: {
         xs: '0px 1px 1px 0px rgba(0, 11, 23, 0.06)',
         sm: '0px 3px 4px 0px rgba(0, 11, 23, 0.04)',
