80b76e231d6924c927f30cdb40490090f93aca96
reza <rezasiahi8@gmail.com>
2024-09-26 10:40:26 +0330

    feat: use cn for merge classname in tailwind

--- libs/design-system/src/lib/components/Breadcrumb/Breadcrumb.tsx
+++ libs/design-system/src/lib/components/Breadcrumb/Breadcrumb.tsx
@@ -1,18 +1,23 @@
+import { cn } from '../../../utils';
 import { Icon } from '../IconComponent';
 import { BreadcrumbItem } from './Breadcrumb.types';
 
-interface Props {
+interface BreadcrumbItems {
   items: BreadcrumbItem[];
 }
 
-export function Breadcrumb({ items }: Props) {
+export function Breadcrumb({ items }: BreadcrumbItems) {
   return (
-    <div dir="rtl" className={`flex items-center gap-1`}>
+    <div dir="rtl" className="flex items-center gap-1">
       {items.map((item: BreadcrumbItem, index: number) => (
         <div
-          className={`cursor-pointer font-semibold flex items-center 
-          ${index + 1 !== items.length && 'hover:text-gray-700'} 
-          ${index + 1 === items.length ? 'text-gray-1000' : 'text-gray-600'}`}
+          className={cn(
+            `cursor-pointer font-semibold flex items-center 
+              ${index + 1 !== items.length && 'hover:text-gray-700'} 
+              ${
+                index + 1 === items.length ? 'text-gray-1000' : 'text-gray-600'
+              }`
+          )}
           key={item.title}
         >
           <div className="flex items-center">
