f4aa942ae2653841591aef66d3eeeb75fcc11723
reza <rezasiahi8@gmail.com>
2024-09-26 12:02:44 +0330

    fix: rename type name prop and use appropriate

--- libs/design-system/src/lib/components/Breadcrumb/Breadcrumb.tsx
+++ libs/design-system/src/lib/components/Breadcrumb/Breadcrumb.tsx
@@ -2,27 +2,28 @@ import { cn } from '../../../utils';
 import { Icon } from '../IconComponent';
 import { BreadcrumbItem } from './Breadcrumb.types';
 
-interface BreadcrumbItems {
+interface Props {
   items: BreadcrumbItem[];
 }
 
-export function Breadcrumb({ items }: BreadcrumbItems) {
+export function Breadcrumb({ items }: Props) {
   return (
     <div dir="rtl" className="flex items-center gap-1">
       {items.map((item: BreadcrumbItem, index: number) => (
         <div
           className={cn(
-            `cursor-pointer font-semibold flex items-center 
-              ${index + 1 !== items.length && 'hover:text-gray-700'} 
-              ${
-                index + 1 === items.length ? 'text-gray-1000' : 'text-gray-600'
-              }`
+            'cursor-pointer font-semibold flex items-center',
+            `${
+              index + 1 === items.length
+                ? 'text-gray-1000'
+                : 'text-gray-600 hover:text-gray-700'
+            }`
           )}
-          key={item.title}
+          key={index}
         >
           <div className="flex items-center">
             <Icon name="house" size="sm" />
-            <p className="text-xs pb-1 mr-0.5 ml-0.5">{item.title}</p>
+            <span className="text-xs pb-1 mr-0.5 ml-0.5">{item.title}</span>
             {index + 1 !== items.length && (
               <Icon name="chevron-left" size="sm" />
             )}
