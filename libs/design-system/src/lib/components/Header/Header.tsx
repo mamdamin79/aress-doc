bcb9ce2df72bacadde6e100d06bd40ccf620250d
reza <rezasiahi8@gmail.com>
2024-09-29 10:00:46 +0330

    feat: create custom toast in react-hot-toast

--- libs/design-system/src/lib/components/Header/Header.tsx
+++ libs/design-system/src/lib/components/Header/Header.tsx
@@ -1,5 +1,29 @@
 import { cn } from '../../../utils';
+import { useCustomToast } from '../../../hooks/CustomToast';
+import { Toaster } from 'react-hot-toast';
 
 export function Header() {
-  return <header className={cn('ss', 'ff')}>This is header!</header>;
+  const { info, success, error, warning } = useCustomToast();
+
+  return (
+    <header dir="rtl" className={cn('ss', 'ff flex flex-col w-fit')}>
+      <button onClick={() => info('حالت اطلاع رسانی')}>info</button>
+      <button onClick={() => success('رمز یک‌بار مصرف برای شما ارسال شد.')}>
+        success
+      </button>
+      <button onClick={() => error('شماره همراه یا رمز عبور نادرست است.')}>
+        error
+      </button>
+      <button
+        onClick={() =>
+          warning(
+            'بعد از اتمام زمان‌بندی میتوانید برای ارسال مجدد رمز یک‌بار مصرف اقدام کنید.'
+          )
+        }
+      >
+        warning
+      </button>
+      <Toaster />
+    </header>
+  );
 }
