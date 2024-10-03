da7b9598e3559956c2539c25334bbed92f6a2895
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-10-03 13:40:42 +0330

    feat: develop styles for accordion icon

--- libs/design-system/src/lib/components/Accordion/Accordion.tsx
+++ libs/design-system/src/lib/components/Accordion/Accordion.tsx
@@ -7,22 +7,33 @@ import {
 import { accordionPropsType } from './Accordion.types';
 import { Icon } from '../IconComponent';
 
-export const Accordion: React.FC<accordionPropsType> = ({ items }) => {
-  return items.map((item) => (
+export const Accordion: React.FC<accordionPropsType> = ({
+  items,
+  singleOpen,
+}) => {
+  return items.map((item, index) => (
     <Disclosure
       as="div"
+      key={item.title}
       className="w-full border p-6 rounded-lg border-gray-300 mb-2"
     >
-      {' '}
-      <DisclosureButton className="flex w-full items-center justify-between ">
-        <span className="text-gray-1000 text-md">{item.title}</span>
-        <span className="bg-brand-600 text-white rounded-full flex items-center justify-center p-1">
-          <Icon name="chevron-down" size="lg" />
-        </span>
-      </DisclosureButton>
-      <DisclosurePanel className="border-t border-gray-300 text-sm text-gray-500 mt-4 pt-4">
-        {item.content}
-      </DisclosurePanel>
+      {({ open }) => (
+        <>
+          <DisclosureButton className=" flex w-full items-center justify-between ">
+            <span className="text-gray-1000 text-md">{item.title}</span>
+            <span
+              className={`bg-brand-600 text-white rounded-full flex items-center justify-center p-1 transition-transform duration-300 ${
+                open ? 'rotate-180' : 'rotate-0'
+              }`}
+            >
+              <Icon name="chevron-down" size="lg" />
+            </span>
+          </DisclosureButton>
+          <DisclosurePanel className="border-t border-gray-300 text-sm text-gray-500 mt-4 pt-4">
+            {item.content}
+          </DisclosurePanel>
+        </>
+      )}
     </Disclosure>
   ));
 };
