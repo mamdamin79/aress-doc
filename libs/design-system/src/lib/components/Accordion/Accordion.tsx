71baee6164adba6ab90de1a782e7b5cc51076217
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-10-03 15:04:34 +0330

    feat: add single open ability

--- libs/design-system/src/lib/components/Accordion/Accordion.tsx
+++ libs/design-system/src/lib/components/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Disclosure,
   DisclosureButton,
@@ -11,27 +11,45 @@ export const Accordion: React.FC<accordionPropsType> = ({
   items,
   singleOpen,
 }) => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
   return items.map((item, index) => (
     <Disclosure
       as="div"
       key={item.title}
+      onClick={() => setOpenIndex((prev) => (prev === index ? null : index))}
       className="w-full border p-6 rounded-lg border-gray-300 mb-2"
     >
       {({ open }) => (
         <>
-          <DisclosureButton className=" flex w-full items-center justify-between ">
-            <span className="text-gray-1000 text-md">{item.title}</span>
-            <span
-              className={`bg-brand-600 text-white rounded-full flex items-center justify-center p-1 transition-transform duration-300 ${
-                open ? 'rotate-180' : 'rotate-0'
-              }`}
-            >
-              <Icon name="chevron-down" size="lg" />
-            </span>
-          </DisclosureButton>
-          <DisclosurePanel className="border-t border-gray-300 text-sm text-gray-500 mt-4 pt-4">
-            {item.content}
-          </DisclosurePanel>
+          {open ? (
+            <button className=" flex w-full items-center justify-between ">
+              <span className="text-gray-1000 text-md">{item.title}</span>
+              <span
+                className={`bg-brand-600  text-white rounded-full flex items-center justify-center p-1 transition-transform duration-300 ${
+                  open && openIndex === index ? 'rotate-180' : 'rotate-0'
+                }`}
+              >
+                <Icon name="chevron-down" size="lg" />
+              </span>
+            </button>
+          ) : (
+            <DisclosureButton className=" flex w-full items-center justify-between ">
+              <span className="text-gray-1000 text-md">{item.title}</span>
+              <span
+                className={`bg-brand-600 text-white rounded-full flex items-center justify-center p-1 transition-transform duration-300 ${
+                  open ? 'rotate-180' : 'rotate-0'
+                }`}
+              >
+                <Icon name="chevron-down" size="lg" />
+              </span>
+            </DisclosureButton>
+          )}
+          {openIndex === index && (
+            <DisclosurePanel className="border-t border-gray-300 text-sm text-gray-500 mt-4 pt-4">
+              {item.content}
+            </DisclosurePanel>
+          )}
         </>
       )}
     </Disclosure>
