7fcfddca7e3ded3a068b08bce1f4d796cf5a2484
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-10-03 12:04:45 +0330

    feat: add style to accordion

--- libs/design-system/src/lib/components/Accordion/Accordion.tsx
+++ libs/design-system/src/lib/components/Accordion/Accordion.tsx
@@ -5,17 +5,24 @@ import {
   DisclosurePanel,
 } from '@headlessui/react';
 import { accordionPropsType } from './Accordion.types';
+import { Icon } from '../IconComponent';
 
-export const Accordion: React.FC<accordionPropsType> = () => {
-  return (
-    <Disclosure>
-      <DisclosureButton className="py-2">
-        Is team pricing available?
+export const Accordion: React.FC<accordionPropsType> = ({ items }) => {
+  return items.map((item) => (
+    <Disclosure
+      as="div"
+      className="w-full border p-6 rounded-lg border-gray-300 mb-2"
+    >
+      {' '}
+      <DisclosureButton className="flex w-full items-center justify-between ">
+        <span className="text-gray-1000 text-md">{item.title}</span>
+        <span className="bg-brand-600 text-white rounded-full flex items-center justify-center p-1">
+          <Icon name="chevron-down" size="lg" />
+        </span>
       </DisclosureButton>
-      <DisclosurePanel className="text-gray-500">
-        Yes! You can purchase a license that you can share with your entire
-        team.
+      <DisclosurePanel className="border-t border-gray-300 text-sm text-gray-500 mt-4 pt-4">
+        {item.content}
       </DisclosurePanel>
     </Disclosure>
-  );
+  ));
 };
