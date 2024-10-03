86c9ffb69cc100d55597a9f0202891c2c7bfe4d1
reza <rezasiahi8@gmail.com>
2024-10-03 11:38:39 +0330

    feat: use storybook for ProgressBar

---
+++ libs/design-system/src/lib/components/ProgressBar/ProgressBar.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ProgressBar } from './ProgressBar';
+
+// Meta configuration for the ProgressBar component in Storybook
+const meta: Meta<typeof ProgressBar> = {
+  title: 'Components/ProgressBar', // Defines the title in Storybook's UI
+  component: ProgressBar, // Links to the actual component
+  tags: ['autodocs'], // Optional: Add any additional tags for better categorization
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProgressBar>;
+
+// A default story for the Header component
+export const Default: Story = {
+  args: {
+    progressBarItems: [
+      'کد ملی و شماره همراه',
+      'رمز یک‌بار مصرف',
+      'رمز عبور جدید',
+    ],
+    activeIndex: 0,
+  },
+};
