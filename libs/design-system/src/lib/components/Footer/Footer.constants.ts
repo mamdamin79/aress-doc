9d0230ae23b6c8b0c2bf7c1978e60cfe52ca45d1
reza <rezasiahi8@gmail.com>
2024-10-05 08:53:08 +0330

    feat: export all the icons related to the footer

---
+++ libs/design-system/src/lib/components/Footer/Footer.constants.ts
@@ -0,0 +1,19 @@
+import logo from './../../../public/icons/logo.svg';
+import enamad from './../../../public/icons/enamad.svg';
+import electronik from './../../../public/icons/electronik.svg';
+import telegram from './../../../public/icons/telegram.svg';
+import android from './../../../public/icons/android.svg';
+import bazar from './../../../public/icons/bazar.svg';
+import mayket from './../../../public/icons/mayket.svg';
+import playstore from './../../../public/icons/playstore.svg';
+
+export {
+  logo,
+  enamad,
+  electronik,
+  telegram,
+  android,
+  bazar,
+  mayket,
+  playstore,
+};
