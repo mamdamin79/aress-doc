bec0fa538b655ac070d5d541d95675df5b1b4f4f
reza <rezasiahi8@gmail.com>
2024-10-06 08:56:11 +0330

    refactor: delete additional comments in Footer

--- libs/design-system/src/lib/components/Footer/Footer.constants.tsx
+++ libs/design-system/src/lib/components/Footer/Footer.constants.tsx
@@ -1,4 +1,3 @@
-// Importing various icons from the public directory
 import logo from './../../../public/icons/logo.svg';
 import enamad from './../../../public/icons/enamad.svg';
 import electronik from './../../../public/icons/electronik.svg';
@@ -7,12 +6,9 @@ import android from './../../../public/icons/android.svg';
 import bazar from './../../../public/icons/bazar.svg';
 import mayket from './../../../public/icons/mayket.svg';
 import playstore from './../../../public/icons/playstore.svg';
-
-// Importing types for FooterSection and Icon component
 import { FooterSection } from './Footer.types';
 import { Icon } from '../IconComponent';
 
-// Defining the footer links structure
 export const linksFooter: FooterSection[] = [
   {
     title: 'انواع صندوق ها',
@@ -57,7 +53,7 @@ export const linksFooter: FooterSection[] = [
     ],
   },
 ];
-// Exporting imported icons for use in other components
+
 export {
   logo,
   enamad,
