c0c7cabb278caadb2270ca3b9df6b688be0d2c6b
reza <rezasiahi8@gmail.com>
2024-10-07 08:47:41 +0330

    fix: remove side spacing

--- libs/design-system/src/lib/components/ProgressBar/ProgressBar.tsx
+++ libs/design-system/src/lib/components/ProgressBar/ProgressBar.tsx
@@ -8,15 +8,44 @@ interface Props {
 
 export function ProgressBar({ progressBarItems, activeIndex }: Props) {
   return (
-    <div className="relative flex mx-auto items-start justify-between">
-      {activeIndex < progressBarItems.length &&
-        activeIndex >= 0 &&
-        progressBarItems.map((item: string, index: number) => (
-          <div
-            className={cn(
-              index === progressBarItems.length - 1 ? 'w-full' : 'w-full',
-              'relative flex flex-col justify-center items-center'
+    <div className="flex px-20 justify-between items-center">
+      <div className="w-full  flex items-center">
+        {progressBarItems.map((item, index) => (
+          <>
+            <div className="relative flex flex-col items-center justify-center">
+              {activeIndex === index ? (
+                <ProgressCircle mode="active" />
+              ) : activeIndex > index ? (
+                <ProgressCircle mode="inactive" />
+              ) : (
+                <ProgressCircle mode="passed" />
+              )}
+              <div
+                className={cn(
+                  'absolute w-40 mr-4 font-vazirmatn text-md top-0 text-center mt-5 break-all',
+                  activeIndex < index && 'text-gray-600 text-sm'
+                )}
+              >
+                {item}
+              </div>
+            </div>
+            {index + 1 < progressBarItems.length && (
+              <div className="w-full -z-50 overflow-hidden relative h-2">
+                <div className="w-full h-full bg-gray-200 absolute"></div>
+                <div
+                  className={cn(
+                    'w-full h-full absolute',
+                    activeIndex > index && 'bg-brand-600 animate-progressBar'
+                  )}
+                ></div>
+              </div>
             )}
+          </>
+        ))}
+
+        {/* {progressBarItems.map((item: string, index: number) => (
+          <div
+            className="flex flex-col items-center relative"
             key={index}
           >
             {activeIndex === index ? (
@@ -27,11 +56,11 @@ export function ProgressBar({ progressBarItems, activeIndex }: Props) {
               <ProgressCircle mode="passed" />
             )}
             {index + 1 < progressBarItems.length && (
-              <div className="w-full -z-50 overflow-hidden relative h-2 mr-[100%]">
+              <div className="w-full -z-50 overflow-hidden relative h-2">
                 <div className="w-full h-full bg-gray-200 absolute"></div>
                 <div
                   className={cn(
-                    'w-full h-full absolute',
+                    'w-full h-full',
                     activeIndex > index && 'bg-brand-600 animate-progressBar'
                   )}
                 ></div>
@@ -39,14 +68,17 @@ export function ProgressBar({ progressBarItems, activeIndex }: Props) {
             )}
             <span
               className={cn(
-                'text-md mt-4 mr-4',
+                'text-md absolute top-0 mt-16 text-center mr-4',
                 activeIndex < index && 'text-gray-600 text-sm'
               )}
             >
               {item}
+            <div className='border-t-2 flex-auto bg-red-600 w-[100%] absolute'>
+            </div>
             </span>
           </div>
-        ))}
+        ))} */}
+      </div>
     </div>
   );
 }
