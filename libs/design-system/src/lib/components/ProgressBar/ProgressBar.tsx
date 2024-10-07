0bf2bf412a24cf3507ada887c47c6581ee143fc4
reza <rezasiahi8@gmail.com>
2024-10-07 09:18:51 +0330

    fix: remove comment code ProgressBar

--- libs/design-system/src/lib/components/ProgressBar/ProgressBar.tsx
+++ libs/design-system/src/lib/components/ProgressBar/ProgressBar.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function ProgressBar({ progressBarItems, activeIndex }: Props) {
   return (
     <div className="flex px-20 py-10 justify-between items-center">
-      <div className="w-full  flex items-center">
+      <div className="w-full flex items-center">
         {progressBarItems.map((item, index) => (
           <>
             <div className="relative flex flex-col items-center justify-center">
@@ -42,42 +42,6 @@ export function ProgressBar({ progressBarItems, activeIndex }: Props) {
             )}
           </>
         ))}
-
-        {/* {progressBarItems.map((item: string, index: number) => (
-          <div
-            className="flex flex-col items-center relative"
-            key={index}
-          >
-            {activeIndex === index ? (
-              <ProgressCircle mode="active" />
-            ) : activeIndex > index ? (
-              <ProgressCircle mode="inactive" />
-            ) : (
-              <ProgressCircle mode="passed" />
-            )}
-            {index + 1 < progressBarItems.length && (
-              <div className="w-full -z-50 overflow-hidden relative h-2">
-                <div className="w-full h-full bg-gray-200 absolute"></div>
-                <div
-                  className={cn(
-                    'w-full h-full',
-                    activeIndex > index && 'bg-brand-600 animate-progressBar'
-                  )}
-                ></div>
-              </div>
-            )}
-            <span
-              className={cn(
-                'text-md absolute top-0 mt-16 text-center mr-4',
-                activeIndex < index && 'text-gray-600 text-sm'
-              )}
-            >
-              {item}
-            <div className='border-t-2 flex-auto bg-red-600 w-[100%] absolute'>
-            </div>
-            </span>
-          </div>
-        ))} */}
       </div>
     </div>
   );
