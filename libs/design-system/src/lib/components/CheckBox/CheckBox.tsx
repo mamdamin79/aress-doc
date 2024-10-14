74afb4955f70fc2d916b9fa10565ff5afdf5a280
reza <rezasiahi8@gmail.com>
2024-10-14 14:35:44 +0330

    refactor: use label for checkbox checked

--- libs/design-system/src/lib/components/CheckBox/CheckBox.tsx
+++ libs/design-system/src/lib/components/CheckBox/CheckBox.tsx
@@ -1,19 +1,23 @@
-import { Checkbox } from '@headlessui/react';
+import { Checkbox, Field, Label } from '@headlessui/react';
 import { Icon } from '../Icon';
 import { cn } from '../../../utils';
+import { useId } from 'react';
 
 interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   onChange: () => void;
 }
 
 export function CheckBoxComponent(props: CheckboxProps) {
+  const unikId = useId();
+
   return (
-    <div className="flex items-center gap-2">
+    <Field className="flex items-center gap-2">
       <Checkbox
         checked={props.checked}
         onChange={props.onChange}
         disabled={props.disabled}
         className="group"
+        id={props.id ?? unikId}
       >
         <div className="border group-data-[disabled]:cursor-default flex items-center justify-center cursor-pointer group-data-[checked]:bg-brand-600 group-data-[disabled]:border-gray-400 group-data-[checked]:border-brand-600 rounded-sm w-fit border-gray-600 min-w-6 min-h-6">
           <div className="hidden text-white group-data-[checked]:block">
@@ -21,7 +25,8 @@ export function CheckBoxComponent(props: CheckboxProps) {
           </div>
         </div>
       </Checkbox>
-      <p
+      <Label
+        htmlFor={props.id ?? unikId}
         className={cn(
           { 'text-gray-400 cursor-default': props.disabled },
           { 'text-gray-1000': props.checked && !props.disabled },
@@ -30,7 +35,7 @@ export function CheckBoxComponent(props: CheckboxProps) {
         )}
       >
         {props.content}
-      </p>
-    </div>
+      </Label>
+    </Field>
   );
 }
