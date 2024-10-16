6d6c4c5b9c201860ada946f424dc93ba234c8265
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-10-16 13:35:03 +0330

    refactor: rafactor paths for src of svgs

--- libs/design-system/src/lib/components/Icon/CustomIcon/CustomIcon.contants.ts
+++ libs/design-system/src/lib/components/Icon/CustomIcon/CustomIcon.contants.ts
@@ -1,15 +1,15 @@
 import { SVGProps } from 'react';
-import { ReactComponent as CustomCalendar } from '../src/icons/CustomCalendar.svg';
-import { ReactComponent as CustomBag } from '../src/icons/CustomBag.svg';
-import { ReactComponent as CustomBadge } from '../src/icons/CustomBadge.svg';
-import { ReactComponent as CustomBeta } from '../src/icons/CustomBeta.svg';
-import { ReactComponent as CustomCircleSlice } from '../src/icons/CustomCircleSlice.svg';
-import { ReactComponent as CustomWallet } from '../src/icons/CustomWallet.svg';
-import { ReactComponent as CustomScalesOfJustice } from '../src/icons/CustomScalesOfJustice.svg';
-import { ReactComponent as CustomClock } from '../src/icons/CustomClock.svg';
-import { ReactComponent as CustomCircularUser } from '../src/icons/CustomCircularUser.svg';
-import { ReactComponent as CustomAlpha } from '../src/icons/CustomAlpha.svg';
-import { ReactComponent as CustomClose } from '../src/icons/CustomClose.svg';
+import { ReactComponent as CustomCalendar } from './src/icons/CustomCalendar.svg';
+import { ReactComponent as CustomBag } from './src/icons/CustomBag.svg';
+import { ReactComponent as CustomBadge } from './src/icons/CustomBadge.svg';
+import { ReactComponent as CustomBeta } from './src/icons/CustomBeta.svg';
+import { ReactComponent as CustomCircleSlice } from './src/icons/CustomCircleSlice.svg';
+import { ReactComponent as CustomWallet } from './src/icons/CustomWallet.svg';
+import { ReactComponent as CustomScalesOfJustice } from './src/icons/CustomScalesOfJustice.svg';
+import { ReactComponent as CustomClock } from './src/icons/CustomClock.svg';
+import { ReactComponent as CustomCircularUser } from './src/icons/CustomCircularUser.svg';
+import { ReactComponent as CustomAlpha } from './src/icons/CustomAlpha.svg';
+import { ReactComponent as CustomClose } from './src/icons/CustomClose.svg';
 
 export const CUSTOM_ICONS: Record<string, React.FC<SVGProps<SVGSVGElement>>> = {
   CustomCalendar,
