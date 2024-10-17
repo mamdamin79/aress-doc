0307312fa9442b5787cf9431eea4e970e6d8c548
reza <rezasiahi8@gmail.com>
2024-10-17 09:53:20 +0330

    refactor: remove additional comments for types footer

--- libs/design-system/src/lib/components/Footer/Footer.types.ts
+++ libs/design-system/src/lib/components/Footer/Footer.types.ts
@@ -1,15 +1,14 @@
-// IconType represents an icon with an optional link.
 export interface IconType {
   icon: React.ReactNode;
   link?: string;
 }
-// FooterLink defines a single link item in the footer.
+
 export interface FooterLink {
   title?: string;
   link?: string;
   icons?: IconType[];
 }
-// FooterSection represents a section of the footer.
+
 export interface FooterSection {
   title: string;
   links: FooterLink[];
