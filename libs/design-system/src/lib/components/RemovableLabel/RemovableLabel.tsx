c1b4099a45fe1f164b74116ff0198435a6f37df7
reza <rezasiahi8@gmail.com>
2024-10-22 14:03:16 +0330

    refactor: change size icon removable label

--- libs/design-system/src/lib/components/RemovableLabel/RemovableLabel.tsx
+++ libs/design-system/src/lib/components/RemovableLabel/RemovableLabel.tsx
@@ -1,3 +1,4 @@
+import { X } from 'lucide-react';
 import { Icon } from '../Icon';
 
 interface Props {
@@ -8,12 +9,12 @@ interface Props {
 
 export function RemovableLabel({ label, onClose, item }: Props) {
   return (
-    <div className="bg-gray-100 cursor-pointer w-fit group font-vazirmatn rounded-sm p-1 flex items-center justify-center gap-2">
+    <div className="bg-gray-100 cursor-pointer w-fit group font-vazirmatn rounded-sm p-1 flex items-center justify-center gap-1">
       <button
         onClick={() => onClose(item)}
-        className="text-gray-600 w-5 h-5 flex items-center justify-center group-hover:bg-red-600 group-hover:border-red-600 group-hover:text-white duration-300 border-2 border-gray-600 rounded-sm"
+        className="text-gray-600 w-5 h-5 flex items-center justify-center group-hover:bg-red-600 group-hover:border-red-600 group-hover:text-white duration-300 border-2 border-gray-600 rounded-[4px]"
       >
-        <Icon name="x" size="lg" />
+        <X strokeWidth={4} width={12} />
       </button>
       <span className="text-sm text-black">{label}</span>
     </div>
