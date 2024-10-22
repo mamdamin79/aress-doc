fb8b8fb686156865df2378e965ee28356f7d7cbd
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-10-22 11:11:43 +0330

    feat: create structure and write types for context menu

---
+++ libs/design-system/src/lib/components/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ContextMenuItem } from './ContextMenu.types';
+
+type Props = {
+  items: ContextMenuItem[];
+};
+
+export const ContextMenu: React.FC<Props> = ({ items }) => {
+  return <></>;
+};
