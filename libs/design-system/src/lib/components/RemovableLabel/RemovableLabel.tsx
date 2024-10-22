bb94d005fe07dc6c0a7909e53dc177e43998b586
reza <rezasiahi8@gmail.com>
2024-10-22 15:26:32 +0330

    refactor: set space icon checkbox

--- libs/design-system/src/lib/components/RemovableLabel/RemovableLabel.tsx
+++ libs/design-system/src/lib/components/RemovableLabel/RemovableLabel.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export function RemovableLabel({ label, onClose, item }: Props) {
   return (
-    <div className="bg-gray-100 cursor-pointer w-fit group font-vazirmatn rounded-sm p-1 flex items-center justify-center gap-1">
+    <div className="bg-gray-100 cursor-pointer w-fit group font-vazirmatn rounded-sm p-1 flex items-center justify-center gap-1.5">
       <button
         onClick={() => onClose(item)}
         className="text-gray-600 w-5 h-5 flex items-center justify-center group-hover:bg-red-600 group-hover:border-red-600 group-hover:text-white duration-300 border-2 border-gray-600 rounded-[4px]"
