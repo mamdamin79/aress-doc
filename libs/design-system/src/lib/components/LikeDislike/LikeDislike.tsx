84faceb18d15540291cff8ea04e80f66bdf612e2
sinapir2 <sinapir2@gmail.com>
2024-10-26 10:25:14 +0330

    fix: refactor component

--- libs/design-system/src/lib/components/LikeDislike/LikeDislike.tsx
+++ libs/design-system/src/lib/components/LikeDislike/LikeDislike.tsx
@@ -1,42 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { ThumbsUp, ThumbsDown } from 'lucide-react';
 import { LikeDislikeProps } from './LikeDislike.types';
 import { Icon } from '../Icon';
+
 export type Reaction = 'like' | 'dislike';
-// LikeDislike component receives an initial value, a reaction type, a flag to specify if the user has already reacted and a function to call when the reaction is clicked.
+
+// LikeDislike component receives an initial value, a reaction type, a flag to specify if the user has already reacted, and a function to call when the reaction is clicked.
 const LikeDislike: React.FC<LikeDislikeProps> = ({
   initialValue,
   Reaction,
   reactedBefore,
   onReact,
 }) => {
+  // State to manage the current count of reactions
   const [value, setValue] = useState(initialValue);
+  // State to determine if the user has already reacted
   const [reacted, setReacted] = useState(reactedBefore);
-  // In here we handle the reaction which has been clicked. we update value and reacted states accordingly
-  const handleReaction = () => {
-    if (!reacted) {
-      setValue((prevValue) => prevValue + 1);
-      setReacted(true);
-      onReact(Reaction);
-    } else {
-      setValue((prevValue) => prevValue - 1);
-      setReacted(false);
-      onReact(Reaction);
-    }
-  };
-  const getBackgroundColor = (Reaction: Reaction) => {
+
+  // Function to handle reaction click event
+  const handleReaction = useCallback(() => {
+    setValue((prevValue) => (reacted ? prevValue - 1 : prevValue + 1));
+    setReacted((prevReacted) => !prevReacted);
+    onReact(Reaction);
+  }, [reacted, onReact, Reaction]);
+
+  // Function to get the appropriate background color class based on reaction type and user state
+  const getBackgroundColor = useCallback(() => {
     if (reacted) {
-      switch (Reaction) {
-        case 'like':
-          // we use [&>*] syntax to access all direct childrens of specified parent element
-          return 'text-green-600 [&>*]:fill-green-600';
-        case 'dislike':
-          return 'text-red-600 [&>*]:fill-red-600';
-      }
-    } else {
-      return 'text-gray-700 hover:text-gray-1000';
+      return Reaction === 'like'
+        ? 'text-green-600 [&>*]:fill-green-600'
+        : 'text-red-600 [&>*]:fill-red-600';
     }
-  };
+    return 'text-gray-700 hover:text-gray-1000';
+  }, [reacted, Reaction]);
 
   return (
     <button
@@ -46,13 +42,13 @@ const LikeDislike: React.FC<LikeDislikeProps> = ({
       <div className="font-vazirmatn w-[40px] h-[26px] bg-baseBackground flex justify-center items-center rounded-sm gap-8 group-hover:bg-gray-200 transition-all text-[14px]">
         {value}
       </div>
-      <div className={getBackgroundColor(Reaction)}>
-        {/* showing the reaction icon based on the Reaction prop */}
-        {Reaction === 'like' ? (
-          <Icon name="thumbs-up" key="thumbs-up" size="sm" />
-        ) : (
-          <Icon name="thumbs-down" key="thumbs-up" size="sm" />
-        )}
+      <div className={getBackgroundColor()}>
+        {/* Displaying the appropriate reaction icon based on the Reaction prop */}
+        <Icon
+          name={Reaction === 'like' ? 'thumbs-up' : 'thumbs-down'}
+          key={Reaction}
+          size="sm"
+        />
       </div>
     </button>
   );
