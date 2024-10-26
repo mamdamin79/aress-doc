0dc21882ccbe916e4cde60060846835922e9ca70
reza <rezasiahi8@gmail.com>
2024-10-26 15:46:07 +0330

    refactor: change render progressBar circle component

--- libs/design-system/src/lib/components/ProgressBar/ProgressCircle.tsx
+++ libs/design-system/src/lib/components/ProgressBar/ProgressCircle.tsx
@@ -3,23 +3,26 @@ interface Props {
 }
 
 export function ProgressCircle({ mode }: Props) {
-  return (
-    <div>
-      {mode === 'active' ? (
-        <div className="w-8 h-8 absolute -top-5 bg-brand-300 flex items-center justify-center rounded-full">
-          <div className="rounded-full bg-white border-brand-600 flex items-center justify-center border-2 w-6 h-6">
-            <div className="w-2.5 h-2.5 bg-brand-600 rounded-full"></div>
-          </div>
+  if (mode === 'active')
+    return (
+      <div className="w-8 h-8 absolute -top-5 bg-brand-300 flex items-center justify-center rounded-full">
+        <div className="rounded-full bg-white border-brand-600 flex items-center justify-center border-2 w-6 h-6">
+          <div className="w-2.5 h-2.5 bg-brand-600 rounded-full"></div>
         </div>
-      ) : mode === 'inactive' ? (
-        <div className="w-6 h-6 absolute -top-4 bg-brand-600 rounded-full flex items-center justify-center">
-          <div className="rounded-full bg-white w-2.5 h-2.5"></div>
-        </div>
-      ) : (
-        <div className="rounded-full absolute -top-4 flex items-center justify-center bg-white w-6 h-6 border-gray-300 border-2">
-          <div className="bg-gray-200 rounded-full w-2.5 h-2.5"></div>
-        </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+
+  if (mode === 'inactive')
+    return (
+      <div className="w-6 h-6 absolute -top-4 bg-brand-600 rounded-full flex items-center justify-center">
+        <div className="rounded-full bg-white w-2.5 h-2.5"></div>
+      </div>
+    );
+
+  if (mode === 'passed')
+    return (
+      <div className="rounded-full absolute -top-4 flex items-center justify-center bg-white w-6 h-6 border-gray-300 border-2">
+        <div className="bg-gray-200 rounded-full w-2.5 h-2.5"></div>
+      </div>
+    );
 }
