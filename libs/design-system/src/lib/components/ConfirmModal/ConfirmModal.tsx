2da9210306b6de665f8493d477536a678d278383
sinapir2 <sinapir2@gmail.com>
2024-10-30 14:34:21 +0330

    fix: replace button with Button component

--- libs/design-system/src/lib/components/ConfirmModal/ConfirmModal.tsx
+++ libs/design-system/src/lib/components/ConfirmModal/ConfirmModal.tsx
@@ -11,6 +11,8 @@ import {
 } from '@headlessui/react';
 import { Icon } from '../Icon';
 import { ConfirmModalProps } from './ConfirmModal.types';
+import { CustomIcon } from '../Icon/CustomIcon/CustomIcon';
+import { Button } from '../Button';
 
 const CustomComponent: React.FC<ConfirmModalProps> = ({
   title,
@@ -46,14 +48,12 @@ const CustomComponent: React.FC<ConfirmModalProps> = ({
       <div className="fixed inset-0 flex items-center justify-center p-4">
         <DialogPanel className="relative w-[472px] bg-white shadow-lg rounded-2xl font-vazirmatn flex flex-col gap-4 p-6">
           {/* Close Button */}
-          <div className="absolute top-0 left-0 -mt-2 -ml-2 w-8 h-8 bg-brand-600 rounded-full flex justify-center items-center shadow-sm">
-            <button
-              onClick={handleCancel}
-              className="bg-white rounded-full shadow-lg w-6 h-6 flex justify-center items-center"
-              aria-label="Close"
-            >
-              <Icon name="x" key="x" size="md" />
-            </button>
+          <div className="absolute top-0 left-0 -mt-2 -ml-2 flex justify-center items-center shadow-sm rounded-full">
+            <CustomIcon
+              name="CustomCirlcleX"
+              key={`customcirclex`}
+              size="lg_plus"
+            />
           </div>
           {/* Dialog Title */}
           <DialogTitle className="text-lg font-semibold h-8">
@@ -105,23 +105,34 @@ const CustomComponent: React.FC<ConfirmModalProps> = ({
           )}
           {/* Handle Submit or Cancel */}
           <div className="flex justify-end gap-2">
-            <button
-              onClick={handleCancel}
-              className="flex justify-center items-center py-2 px-4 gap-2 border-2 border-brand-600 rounded-md text-brand-600 text-base leading-6 text-center hover:bg-gray-100 transition-colors"
-            >
-              {CancelButtonText}
-            </button>
-            <button
-              onClick={handleConfirm}
-              disabled={Boolean(!inputValue && input?.placeholder)}
-              className={`${
-                !inputValue && input?.placeholder
-                  ? 'bg-brand-300 cursor-not-allowed'
-                  : 'bg-brand-600 hover:bg-brand-700'
-              } flex justify-center items-center py-2 px-4 gap-2 rounded-md text-white text-base leading-6 text-center transition-colors`}
-            >
-              {ConfirmButtonText}
-            </button>
+            <div className="min-w-[80px]">
+              <Button
+                align="center"
+                isLoading={false}
+                size="sm"
+                mode="secondary"
+                onClick={handleCancel}
+              >
+                {CancelButtonText}
+              </Button>
+            </div>
+            <div className="min-w-[80px]">
+              <Button
+                align="center"
+                isLoading={false}
+                mode="primary"
+                size="sm"
+                onClick={handleConfirm}
+                disabled={Boolean(!inputValue && input?.placeholder)}
+                className={`${
+                  !inputValue && input?.placeholder
+                    ? 'bg-brand-300 cursor-not-allowed'
+                    : 'bg-brand-600 hover:bg-brand-700'
+                }`}
+              >
+                {ConfirmButtonText}
+              </Button>
+            </div>
           </div>
         </DialogPanel>
       </div>
