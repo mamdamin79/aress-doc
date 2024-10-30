c86a686832263020b441cf0ee770f7d778bcee2f
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-10-30 16:10:55 +0330

    feat: render a video without any control on it in story book

---
+++ libs/design-system/src/lib/components/VideoPlayer/VideoPlayer.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { VideoPlayer } from './VideoPlayer';
+const meta: Meta<typeof VideoPlayer> = {
+  component: VideoPlayer,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof VideoPlayer>;
+
+export const Default: Story = {
+  args: {
+    src:"https://classbon.arvanvod.ir/0q36Dx65Pj/5q0AvBAQKk/origin_d3plBgkPEaUzlSbwtzk1Sc1N6z9RhQDdzYAgMXEC.mp4"
+  },
+};
--- libs/design-system/src/lib/components/VideoPlayer/VideoPlayer.tsx
+++ libs/design-system/src/lib/components/VideoPlayer/VideoPlayer.tsx
@@ -11,8 +11,10 @@ export const VideoPlayer : React.FC<Prosp> = ({
     src,
     poster=""
 })=>{
-    const {play,videoRef,pause} = useVideo(src)
+    const {play,videoRef,pause,currentTime,isVideoLoaded,isVideoWaited,fullScreen,duration} = useVideo(src)
     return(
-        <></>
+        <div className="relative">
+            <video src={src} className="w-full" poster={poster} ref={videoRef} />
+        </div>
     )
 }
