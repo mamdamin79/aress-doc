13813fdddbc0e53df891992715563618d0a9e18f
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-10-30 17:02:00 +0330

    feat: writing control pannel of video player

--- libs/design-system/src/lib/components/VideoPlayer/VideoPlayer.tsx
+++ libs/design-system/src/lib/components/VideoPlayer/VideoPlayer.tsx
@@ -1,5 +1,7 @@
 import { useVideo } from "../../../hooks/UseVideo"
 import React from "react"
+import { Button } from "../Button"
+import { Icon } from "../Icon"
 
 type Prosp = {
     src:string,
@@ -11,10 +13,24 @@ export const VideoPlayer : React.FC<Prosp> = ({
     src,
     poster=""
 })=>{
-    const {play,videoRef,pause,currentTime,isVideoLoaded,isVideoWaited,fullScreen,duration} = useVideo(src)
+    const {play,videoRef,pause,currentTime,isVideoLoaded,isVideoWaited,fullScreen,duration,progress,isPlaying} = useVideo(src)
     return(
         <div className="relative">
             <video src={src} className="w-full" poster={poster} ref={videoRef} />
+            <div className="absolute w-full bg-red-200 h-20 flex flex-col gap-4 bottom-4 px-8 ">
+                {/* progress bar */}
+                    {/* <progress value={progress}></progress> */}
+                    <div className="w-full bg-blue-200 h-5"></div>
+                {/* controls */}
+                <div className="flex items-center justify-between " dir="ltr">
+                    <div className="flex items-center gap-4">
+                        <Button mode="primary" size="sm" isLoading={false} align="center"><Icon name="skip-back"/></Button>
+                        {isPlaying ? <Button onClick={pause} mode="primary" size="sm" isLoading={false} align="center"><Icon name="pause"/></Button>: <Button onClick={play} mode="primary" size="sm" isLoading={false} align="center"><Icon name="play"/></Button>}
+                        <Button mode="primary" size="sm" isLoading={false} align="center"><Icon name="skip-forward"/></Button>
+                        <Button mode="primary" size="sm" isLoading={false} align="center"><Icon name="volume-2"/></Button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- libs/design-system/src/utils/index.ts
+++ libs/design-system/src/utils/index.ts
@@ -1,1 +1,3 @@
 export * from './classNames.utils';
+export * from './time';
+
