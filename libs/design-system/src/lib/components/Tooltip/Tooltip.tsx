ada6a5fa934b4f08d4508619553063b7bd5f5296
reza <rezasiahi8@gmail.com>
2024-11-11 13:55:57 +0330

    feat: install package

---
+++ libs/design-system/src/lib/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,43 @@
+import { cn } from '../../../utils/classNames.utils';
+import React, { useId } from 'react';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
+
+type Props = {
+  children: React.ReactNode;
+  title: string;
+  position?: 'top' | 'right' | 'bottom' | 'left';
+  className?: string;
+};
+
+export const Tooltip: React.FC<Props> = ({
+  children,
+  title,
+  className,
+  position = 'top',
+}) => {
+  const id = useId();
+  return (
+    <>
+      {/* wrapper */}
+      <div data-tooltip-id={id} className="relative cursor-pointer">
+        {/* here is the component that need a tooltip */}
+        {children}
+      </div>
+      <ReactTooltip
+        id={id}
+        noArrow
+        place={position}
+        positionStrategy="fixed"
+        offset={4}
+        content={title}
+        className={cn(
+          'bg-gray-1000 shadow-5xl rounded-xs text-white font-medium font-vazirmatn text-sm',
+          className
+        )}
+      ></ReactTooltip>
+    </>
+  );
+};
+
+export default Tooltip;
