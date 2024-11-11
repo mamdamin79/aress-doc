cc3d88f6ff5a379a276b57640c977eb81387e2aa
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-11-11 12:39:45 +0330

    feat: add a text that shows user wich posts they see

--- libs/design-system/src/lib/components/Pagination/Pagination.stories.tsx
+++ libs/design-system/src/lib/components/Pagination/Pagination.stories.tsx
@@ -1,20 +1,45 @@
-import { Meta, StoryObj } from "@storybook/react";
-import {Pagination} from "./Pagination";
+import { Meta, StoryObj } from '@storybook/react';
+import { Pagination } from './Pagination';
+import { useState } from 'react';
 
 const meta: Meta<typeof Pagination> = {
-  title: "Components/Pagination",
+  title: 'Components/Pagination',
   component: Pagination,
   argTypes: {
-    pageCount: { control: { type: "number", min: 1 } },
-    onPageChange: { action: "page changed" },
+    pageCount: { control: { type: 'number', min: 1 } },
+    onPageChange: { action: 'page changed' },
   },
   args: {
     pageCount: 10,
-    pageSize:5
+    pageSize: 5,
+    currentPage: 5,
   },
 };
 
 export default meta;
+
 type Story = StoryObj<typeof Pagination>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: (args) => {
+    const PaginationWrapper = () => {
+      const [currentPage, setCurrentPage] = useState(args.currentPage);
+
+      const handlePageChange = (selectedItem: { selected: number }) => {
+        setCurrentPage(selectedItem.selected + 1);
+        args.onPageChange(selectedItem); 
+      };
+
+      return (
+        <Pagination
+          {...args}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      );
+    };
+
+    return <PaginationWrapper />;
+  },
+};
+
