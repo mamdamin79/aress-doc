aefb5f9aad9beedecfa6b7ce5bfbe5849bc547cd
reza <rezasiahi8@gmail.com>
2024-11-12 16:42:05 +0330

    feat: add error and brand theme for button

--- libs/design-system/src/lib/components/Button/Button.tsx
+++ libs/design-system/src/lib/components/Button/Button.tsx
@@ -10,10 +10,12 @@ export interface ButtonProps
   size: ButtonSize;
   mode: ButtonMode;
   align: 'center' | 'right';
+  theme: 'brand' | 'error';
 }
 
 export const Button: React.FC<ButtonProps> = ({
   mode,
+  theme = 'brand',
   size,
   disabled,
   align,
@@ -28,47 +30,74 @@ export const Button: React.FC<ButtonProps> = ({
       {...props}
       className={cn(
         'flex gap-2 group w-full transition-all duration-300 items-center px-2',
+        { 'cursor-default': isLoading || disabled },
         { 'justify-center': align === 'center' },
         { 'justify-start': align === 'right' },
         { 'cursor-default': disabled },
         { 'rounded-lg h-12': size === 'md' },
         { 'rounded-md h-[38px]': size === 'sm' },
-        { 'bg-brand-300 text-white': mode === 'primary' && disabled },
-        {
-          'bg-brand-600 text-white':
-            mode === 'primary' && isLoading && !disabled,
-        },
-        {
-          'bg-brand-600 active:bg-brand-800 text-white hover:bg-brand-700':
-            mode === 'primary' && !isLoading && !disabled,
-        },
-        {
-          'border-brand-300 border text-brand-300':
-            mode === 'secondary' && disabled,
-        },
-        {
-          'border border-brand-600 text-brand-600':
-            mode === 'secondary' && isLoading && !disabled,
-        },
-        {
-          'bg-white active:bg-brand-800 border border-brand-600 text-brand-600 hover:bg-brand-700 hover:text-white':
-            mode === 'secondary' && !isLoading && !disabled,
-        },
-        { 'text-brand-300': mode === 'text' && disabled },
-        {
-          'text-brand-600 border border-brand-600':
-            mode === 'text' && isLoading && !disabled,
-        },
-        {
-          'text-brand-600 active:text-brand-800 active:border-brand-800 hover:border hover:border-brand-600':
-            mode === 'text' && !isLoading && !disabled,
-        },
-        { 'text-brand-300': mode === 'underline' && disabled },
-        { 'text-brand-600': mode === 'underline' && isLoading && !disabled },
-        {
-          'text-brand-600 active:text-brand-800 active:border-brand-800':
-            mode === 'underline' && !isLoading && !disabled,
-        }
+        mode === 'primary' &&
+          disabled &&
+          (theme === 'brand'
+            ? 'bg-brand-300 text-white'
+            : 'text-white bg-red-300'),
+        mode === 'primary' &&
+          isLoading &&
+          !disabled &&
+          (theme === 'brand'
+            ? 'bg-brand-600 text-white cursor-default'
+            : 'bg-red-600 cursor-default text-white'),
+        mode === 'primary' &&
+          !isLoading &&
+          !disabled &&
+          (theme === 'brand'
+            ? 'bg-brand-600 active:bg-brand-800 text-white hover:bg-brand-700'
+            : 'bg-red-600 active:bg-red-800 text-white hover:bg-red-700'),
+        mode === 'secondary' &&
+          disabled &&
+          (theme === 'brand'
+            ? 'border-brand-300 border text-brand-300'
+            : 'border-red-300 border text-red-300'),
+        mode === 'secondary' &&
+          isLoading &&
+          !disabled &&
+          (theme === 'brand'
+            ? 'border border-brand-600 text-brand-600'
+            : 'border border-red-600 text-red-600'),
+        mode === 'secondary' &&
+          !isLoading &&
+          !disabled &&
+          (theme === 'brand'
+            ? 'bg-white active:bg-brand-800 border border-brand-600 text-brand-600 hover:bg-brand-700 hover:text-white'
+            : 'bg-white active:bg-red-800 border text-red-600 border-red-600 hover:bg-red-700 hover:text-white'),
+        mode === 'text' &&
+          disabled &&
+          (theme === 'brand' ? 'text-brand-300' : 'text-red-300'),
+        mode === 'text' &&
+          isLoading &&
+          !disabled &&
+          (theme === 'brand'
+            ? 'text-brand-600 border border-brand-600'
+            : 'text-red-600 border border-red-600'),
+        mode === 'text' &&
+          !isLoading &&
+          !disabled &&
+          (theme === 'brand'
+            ? 'text-brand-600 active:text-brand-800 active:border-brand-800 hover:border hover:border-brand-600'
+            : 'text-red-600 active:text-red-800 active:border-red-800 hover:border hover:border-red-600'),
+        mode === 'underline' &&
+          disabled &&
+          (theme === 'brand' ? 'text-brand-300' : 'text-red-300'),
+        mode === 'underline' &&
+          !disabled &&
+          isLoading &&
+          (theme === 'brand' ? 'text-brand-600' : 'text-red-600'),
+        mode === 'underline' &&
+          !isLoading &&
+          !disabled &&
+          (theme === 'brand'
+            ? 'text-brand-600 active:text-brand-800 active:border-brand-800'
+            : 'text-red-600 active:red-brand-800 active:border-red-800')
       )}
     >
       {isLoading ? (
