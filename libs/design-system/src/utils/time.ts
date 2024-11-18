f0e81efdd0244e5163008f0b6545fd1651850adf
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-11-18 16:16:58 +0330

    feat: now the time convertor function doesnt return hour if the duration is less than an hour

--- libs/design-system/src/utils/time.ts
+++ libs/design-system/src/utils/time.ts
@@ -2,11 +2,16 @@ export function secondsToHHMMSS(totalSeconds: number): string {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
-    const formattedHours = hours.toString().padStart(2, "0");
+
     const formattedMinutes = minutes.toString().padStart(2, "0");
     const formattedSeconds = parseInt(seconds.toString(), 10)
         .toString()
         .padStart(2, "0");
 
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    if (hours > 0) {
+        const formattedHours = hours.toString().padStart(2, "0");
+        return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    }
+
+    return `${formattedMinutes}:${formattedSeconds}`;
 }
