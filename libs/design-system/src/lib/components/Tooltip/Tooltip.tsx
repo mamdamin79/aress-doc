9113ce7765f340a420abe1e06e9d119a7be9d439
mamdamin79 <mohammadaminsaheb@gmail.com>
2024-11-18 12:44:55 +0330

    fix: now paddings are true

--- libs/design-system/src/lib/components/Tooltip/Tooltip.tsx
+++ libs/design-system/src/lib/components/Tooltip/Tooltip.tsx
@@ -36,6 +36,7 @@ export const Tooltip: React.FC<Props> = ({
         positionStrategy="fixed"
         offset={offset}
         content={title}
+        style={{padding:"0 8px"}}
         className={cn(
           'bg-gray-1000 shadow-5xl rounded-xs text-white font-medium font-vazirmatn text-sm',
           className
