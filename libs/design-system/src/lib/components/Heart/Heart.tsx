4395495d8f57cb61fcd3b62846e29486ff247e73
sinapir2 <sinapir2@gmail.com>
2024-11-28 10:22:54 +0330

    feat: create heart component

---
+++ libs/design-system/src/lib/components/Heart/Heart.tsx
@@ -0,0 +1,23 @@
+import { cn } from 'libs/design-system/src/utils';
+import { Heart } from 'lucide-react';
+import React from 'react'
+interface HeartProps {
+    isLiked: boolean;
+    onClick: () => void;
+  }
+const HeartComponent = ({ isLiked, onClick }: HeartProps) => {
+  return (
+    <div
+    className={cn(
+      `w-8 h-8 cursor-pointer flex items-center justify-center p-2 rounded-full`,
+    )}
+    onClick={onClick}
+  >
+    <Heart size={24}  strokeWidth={1.8} className={cn(`fill-white text-red-600 transition-all`, isLiked ? 'fill-red-600' : 'hover:text-red-700 hover:fill-red-100')}
+/>
+  </div>
+  )
+}
+export {HeartComponent as Heart}
+
+  
