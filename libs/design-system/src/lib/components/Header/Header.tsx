3c45470fa0acd724ff4167cd3175753a643b781a
reza <rezasiahi8@gmail.com>
2024-12-02 13:19:05 +0330

    fix: test sort classnames

--- libs/design-system/src/lib/components/Header/Header.tsx
+++ libs/design-system/src/lib/components/Header/Header.tsx
@@ -1,5 +1,14 @@
 import { cn } from '../../../utils';
 
 export function Header() {
-  return <header className={cn('ss', 'ff')}>This is header!</header>;
+  return (
+    <header
+      className={cn(
+        'ss',
+        'ff m-6 relative pb-10 top-0 text-right shadow-sm text-md'
+      )}
+    >
+      This is header!
+    </header>
+  );
 }
