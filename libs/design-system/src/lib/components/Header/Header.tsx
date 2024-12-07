0355b0eb0b3cf568b0f71c3e52b25a3c9444897b
reza <rezasiahi8@gmail.com>
2024-12-07 14:04:54 +0330

    refactor: restore to default classname header

--- libs/design-system/src/lib/components/Header/Header.tsx
+++ libs/design-system/src/lib/components/Header/Header.tsx
@@ -1,14 +1,5 @@
 import { cn } from '../../../utils';
 
 export function Header() {
-  return (
-    <header
-      className={cn(
-        'ss',
-        'ff m-6 relative pb-10 top-0 text-right shadow-sm text-md'
-      )}
-    >
-      This is header!
-    </header>
-  );
+  return <header className={cn('ss', 'ff')}>This is header!</header>;
 }
