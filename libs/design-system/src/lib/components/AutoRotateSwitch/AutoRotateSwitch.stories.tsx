41a5d627fbf4b5c9dfd22cd8dc4380182d181d39
sinapir2 <sinapir2@gmail.com>
2024-12-09 10:37:24 +0330

    feat: create auto rotate component ui without animations

---
+++ libs/design-system/src/lib/components/AutoRotateSwitch/AutoRotateSwitch.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react/*';
+import { AutoRotateSwitch } from './AutoRotateSwitch';
+
+const meta: Meta<typeof AutoRotateSwitch> = {
+  title: 'Components/AutoRotateSwitch',
+  component: AutoRotateSwitch,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AutoRotateSwitch>;
+
+export const Default: Story = {
+  render: (args) => (
+    <div className="mt-16 flex w-full justify-center">
+      <AutoRotateSwitch {...args} />
+    </div>
+  ),
+  args: {
+    disabled: false,
+    onChange: () => console.log('onChange'),
+    rotateOptions: [5, 10, 15],
+  },
+};
