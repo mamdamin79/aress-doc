cd9a1f5cf9e216e517045f88870d67bfd52f4e5e
sinapir2 <sinapir2@gmail.com>
2024-12-11 15:44:24 +0330

    fix: persian percentage

--- libs/design-system/src/lib/components/PercentageLabel/PercentageLabel.tsx
+++ libs/design-system/src/lib/components/PercentageLabel/PercentageLabel.tsx
@@ -24,11 +24,11 @@ export const PercentageLabel: React.FC<Props> = ({ value, tooltip, size }) => {
           { 'w-14 text-xs': size === 'small' },
           { 'bg-green-100 text-green-700': value > 0 },
           { 'bg-red-100 text-red-700': value < 0 },
-          { 'text-gray-1000 bg-gray-100': value === 0 }
+          { 'text-gray-1000 bg-gray-100': value === 0 },
         )}
       >
         <div className="flex items-center">
-          <span>{absoluteValue}</span>%
+          <span>{absoluteValue}</span>٪
           <span className="mb-0.5 mr-0.5">
             {value > 0 ? '+' : value < 0 && '-'}{' '}
           </span>
