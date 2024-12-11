cd598d1c6f8907f481b9da64b6f9e51f1b32050f
reza <rezasiahi8@gmail.com>
2024-12-11 13:34:53 +0330

    feat: add search functionality optionslist

--- libs/design-system/src/lib/components/OptionsListExplorer/OptionsListExplorer.tsx
+++ libs/design-system/src/lib/components/OptionsListExplorer/OptionsListExplorer.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { OptionItem, CategoryItem } from './OptionsListExplorer.types';
 import { Icon } from '../Icon';
 import { cn } from '../../../utils';
 import { PercentageLabel } from '../PercentageLabel';
 import { TextField } from '../TextField';
+import { Tabs } from '../Tabs';
 
 interface Props {
   title: string;
@@ -27,23 +28,52 @@ export function OptionsListExplorer({
 }: Props) {
   const [checkedItem, setCheckedItem] = useState(0);
   const [filteredItems, setFilteredItems] = useState<OptionItem[]>(items.items);
+  const [activeTab, setActiveTab] = useState(1);
+  const [inputValue, setInputValue] = useState('');
   const itemsToShow =
     items.items.length > 10 ? filteredItems.slice(0, 3) : filteredItems;
 
   const handlerInput = (value: string) => {
-    onSearch(value);
-    if (value) {
-      const filtredList: OptionItem[] = items.items.filter((item: OptionItem) =>
-        item.title
-          .toLocaleLowerCase()
-          .includes(value.trim().toLocaleLowerCase()),
+    setInputValue(value);
+  };
+
+  useEffect(() => {
+    const filterListByTitle = (title: string) => {
+      return items.items.filter((item: OptionItem) =>
+        item.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()),
       );
-      setFilteredItems(filtredList);
-    } else setFilteredItems(items.items);
+    };
+
+    const filterListByTab = (tab: number) => {
+      return items.items.filter((item: OptionItem) => item.categoryId === tab);
+    };
+    if (activeTab === 1) {
+      if (inputValue.trim()) {
+        setFilteredItems(filterListByTitle(inputValue));
+      } else {
+        setFilteredItems(items.items);
+      }
+    } else if (activeTab > 1) {
+      const listCategory = filterListByTab(activeTab);
+      const filterItems = listCategory.filter((item) =>
+        item.title.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase()),
+      );
+      setFilteredItems(filterItems);
+    }
+  }, [inputValue, activeTab, items.items]);
+
+  const handlerActiveTab = (tabId: number) => {
+    setActiveTab(tabId);
+    setCheckedItem(0);
   };
 
   return (
-    <div className="w-[500px] bg-white py-2.5">
+    <div
+      className={cn(
+        items.items.length > 10 ? 'h-[340px]' : 'h-[290px]',
+        'w-[500px] bg-white',
+      )}
+    >
       <div
         onClick={() => onBackButtonClick}
         className="text-gray-1000 flex w-fit cursor-pointer items-center gap-1 px-4 py-2.5"
@@ -53,8 +83,9 @@ export function OptionsListExplorer({
       </div>
       {!search?.placeholder && <p className="h-0.5 w-full bg-gray-300"></p>}
       {items.items.length > 10 && (
-        <div className="mx-4 h-20 pb-1">
+        <div className="mx-4 h-12 pb-1">
           <TextField
+            value={inputValue}
             onSearchInput={handlerInput}
             placeholder={search?.placeholder}
             mode="outline"
@@ -65,21 +96,22 @@ export function OptionsListExplorer({
         </div>
       )}
       {items.categories && (
-        <div className="mx-4 mb-2 mt-4 flex items-center gap-2">
-          {items.categories.map((item: CategoryItem) => (
-            <span
-              className="cursor-pointer px-3 text-xs font-medium"
-              key={item.title}
-            >
-              {item.title}
-            </span>
-          ))}
+        <div className="mx-4 mt-4">
+          <Tabs
+            tabs={items.categories}
+            bgWhite
+            mode="rounded"
+            onClickTab={(id) => handlerActiveTab(id)}
+          />
         </div>
       )}
       {filteredItems.length ? (
         <div
           className={cn('flex max-h-56 flex-col', {
-            'custom-scrollbar overflow-y-scroll': items.items.length <= 10,
+            'custom-scrollbar h-[60%] overflow-y-scroll':
+              items.items.length <= 10 &&
+              items.items.length > 3 &&
+              filteredItems.length > 3,
           })}
         >
           {itemsToShow.map((item: OptionItem, index: number) => (
@@ -102,7 +134,9 @@ export function OptionsListExplorer({
                 <span className="max-w-52 truncate">{item.title}</span>
               </div>
               <div className="flex items-center gap-1">
-                <p className="w-20 text-center">{item.type}</p>
+                {item.categoryId !== activeTab && (
+                  <p className="w-20 text-center">{item.type}</p>
+                )}
                 <span className="px-4 text-center">{item.priceRials} ریال</span>
                 {typeof item.priceChangePercent === 'number' && (
                   <PercentageLabel
