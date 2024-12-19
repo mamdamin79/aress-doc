6e05a9fdda659c8f8b6618a18311bb9902914499
reza <rezasiahi8@gmail.com>
2024-12-19 13:41:50 +0330

    fix: fix show decimals number formating

--- libs/design-system/src/utils/number-utils.ts
+++ libs/design-system/src/utils/number-utils.ts
@@ -6,9 +6,16 @@ export const formatNumber = (
   num: number | string,
   options: FormatNumberOptions,
 ) => {
+  let fixedNum: number;
+  if (options.decimals !== undefined) {
+    fixedNum =
+      Math.floor(+num * Math.pow(10, options.decimals)) /
+      Math.pow(10, options.decimals);
+  } else fixedNum = +num;
+
   const formatOptions: Intl.NumberFormatOptions = {
     maximumFractionDigits: options?.decimals,
     useGrouping: options?.commaSeparated,
   };
-  return new Intl.NumberFormat('en-IR', formatOptions).format(+num);
+  return new Intl.NumberFormat('en-IR', formatOptions).format(fixedNum);
 };
