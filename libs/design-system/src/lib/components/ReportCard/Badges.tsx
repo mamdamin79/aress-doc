31b0bd18ba4f0fea25ffb79ec8724d4ef539ea08
sinapir2 <sinapir2@gmail.com>
2024-12-25 11:13:27 +0330

    fix: change icon library for badge

--- libs/design-system/src/lib/components/ReportCard/Badges.tsx
+++ libs/design-system/src/lib/components/ReportCard/Badges.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Icon } from '../Icon';
 import { cn } from '../../../utils/classNames.utils';
-import { Heart } from 'lucide-react';
+import { Icon as Iconify } from '@iconify/react';
+import { Icon } from '../Icon';
 
 export const NewBadge = () => {
   return (
-    <div className="w-[37px] h-[22px] flex items-center justify-center text-white text-xs font-medium rounded-[100px] p-2 bg-gradient-to-b from-red-600 to-red-700">
+    <div className="flex h-[22px] w-[37px] items-center justify-center rounded-[100px] bg-gradient-to-b from-red-600 to-red-700 p-2 text-xs font-medium text-white">
       جدید
     </div>
   );
 };
 export const VideoBadge = () => {
   return (
-    <div className=" w-[37px] h-[22px] flex items-center justify-center text-white text-xs font-medium rounded-[100px] p-2 bg-vividGreen-700">
+    <div className="bg-vividGreen-700 flex h-[22px] w-[37px] items-center justify-center rounded-[100px] p-2 text-xs font-medium text-white">
       <Icon name="video" key="video" size="md" />
     </div>
   );
@@ -25,13 +25,17 @@ export const LikeBadge = ({ isLiked, onClick }: LikeBadgeProps) => {
   return (
     <div
       className={cn(
-        `w-[36px] h-[36px] cursor-pointer flex items-center justify-center text-red-600 p-2 bg-white rounded-full
-    hover:bg-red-100 transition-colors`,
-        isLiked ? 'fill-red-600' : ''
+        `flex h-[36px] w-[36px] cursor-pointer items-center justify-center rounded-full bg-white p-2 text-red-600 transition-colors hover:bg-red-100`,
+        isLiked ? 'fill-red-600' : '',
       )}
       onClick={onClick}
     >
-      <Heart width={16} height={16} fill={isLiked ? 'red-600' : 'white'} />
+      <Iconify
+        icon={'lucide:heart'}
+        width={16}
+        height={16}
+        fill={isLiked ? 'red-600' : 'white'}
+      />
     </div>
   );
 };
