ab177f3a8d433a0c96480bc0709c3c9c42f51487
sinapir2 <sinapir2@gmail.com>
2024-12-25 09:11:43 +0330

    fix: implement requested fixes

--- libs/design-system/src/lib/components/AddReportButton/AddReportButton.tsx
+++ libs/design-system/src/lib/components/AddReportButton/AddReportButton.tsx
@@ -7,16 +7,16 @@ export const AddReportButton: React.FC<AddReportButtonProps> = ({
   onClick,
 }) => {
   return (
-    <div
-      className="w-[616px] h-[320px] flex justify-center items-center bg-white rounded-2xl shadow-sm  text-gray-600 text-lg font-medium group hover:text-brand-700"
+    <button
+      className="w-[616px] h-[320px] flex justify-center items-center bg-white rounded-2xl shadow-sm  text-gray-600 text-lg font-medium group/reportBtn hover:text-brand-700"
       onClick={onClick}
     >
       <div className="flex flex-col gap-4 justify-center items-center transition-all">
-        <div className="w-[72px] h-[72px] rounded-full border-2 border-dashed border-gray-500 group-hover:border-brand-700 flex justify-center items-center">
-          <Icon name="plus" key={'plus'} size="lg" />
+        <div className="w-[72px] h-[72px] rounded-full border-2 border-dashed border-gray-500 group-hover/reportBtn:border-brand-700 flex justify-center items-center">
+          <Icon name="plus" size="lg" />
         </div>
         <span className="text-center">افزودن گزارش جدید</span>
       </div>
-    </div>
+    </button>
   );
 };
