4e36b2dfef87c4ab652405dcf315ea6dacd731b6
sinapir2 <sinapir2@gmail.com>
2024-12-25 13:08:22 +0330

    fix: move state handling inside component itself

--- libs/design-system/src/lib/components/Heart/Heart.tsx
+++ libs/design-system/src/lib/components/Heart/Heart.tsx
@@ -1,15 +1,20 @@
 import { cn } from 'libs/design-system/src/utils';
 import { Heart } from 'lucide-react';
-import React from 'react';
+import React, { useState } from 'react';
 interface HeartProps {
-  isLiked: boolean;
-  onClick: () => void;
+  initialIsliked: boolean;
+  onClick: (isLiked: boolean) => void;
 }
-const HeartComponent = ({ isLiked, onClick }: HeartProps) => {
+const HeartComponent = ({ initialIsliked, onClick }: HeartProps) => {
+  const [isLiked, setIsLiked] = useState(initialIsliked);
+  const handleClick = () => {
+    onClick(isLiked);
+    setIsLiked(!isLiked);
+  };
   return (
     <div
       className="w-8 h-8 cursor-pointer flex items-center justify-center p-2 rounded-full"
-      onClick={onClick}
+      onClick={handleClick}
     >
       <Heart
         size={24}
