79d94e1586f4e3deb6f51ca7d10b355dd71d72bd
sinapir2 <sinapir2@gmail.com>
2024-12-25 10:24:42 +0330

    fix: implement requested fixes

--- libs/design-system/src/lib/components/BulletList/BulletList.tsx
+++ libs/design-system/src/lib/components/BulletList/BulletList.tsx
@@ -6,22 +6,22 @@ interface Props {
     title: string;
     status?: 'normal' | 'error' | 'success';
   }[];
-  textColor?: 'gray' | 'black';
+  textColor?: 'semi-dark' | 'dark';
 }
 
-export function BulletList({ items, textColor = 'black' }: Props) {
+export function BulletList({ items, textColor = 'dark' }: Props) {
   return (
-    <div className="w-full">
+    <ul className="w-full">
       {items.map((item, index) => {
         const status = item.status ?? 'normal'; // Default to 'normal'
         return (
-          <div
+          <li
             className={cn(
               'flex items-start gap-2 font-medium',
               status === 'error' && 'text-red-600',
               status === 'success' && 'text-green-600',
               status === 'normal' &&
-                (textColor === 'black'
+                (textColor === 'dark'
                   ? 'text-gray-1000 mr-2'
                   : 'mr-2 text-gray-600'),
             )}
@@ -29,7 +29,7 @@ export function BulletList({ items, textColor = 'black' }: Props) {
           >
             {/* Icon or bullet based on status */}
             {status === 'normal' && (
-              <span className="mt-2.5 rounded-full bg-black p-[2px]"></span>
+              <span className="bg-gray-1000 mt-2.5 h-1 w-1 rounded-full"></span>
             )}
             {status === 'success' && (
               <div className="mt-1">
@@ -40,12 +40,11 @@ export function BulletList({ items, textColor = 'black' }: Props) {
               <div className="mt-1">
                 <Icon name="x" size="sm" />
               </div>
-            )}{' '}
-            {/* Item title */}
+            )}
             {item.title}
-          </div>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 }
