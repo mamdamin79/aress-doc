8881be32d6cbf965c4e38a52aec9d5e3b9260970
sinapir2 <sinapir2@gmail.com>
2024-12-26 14:00:15 +0330

    fix: refactor the code

--- apps/fe-app/app/dashboard/report/page.tsx
+++ apps/fe-app/app/dashboard/report/page.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { Button, Heart, Icon } from 'design-system';
+import { Icon } from 'design-system';
 import Image from 'next/image';
 import React from 'react';
 import fake2 from '../fake2.png';
-import { NewBadge } from 'design-system';
+import { ReportOverview } from './_components/ReportOverview';
 const page = () => {
   return (
     <div className="h-[100vh]">
@@ -22,43 +22,7 @@ const page = () => {
             <span> امکان تغییر تنظیمات پیشفرض پروژه وجود دارد.</span>
           </div>
         </div>
-        <div className="flex w-[600px] flex-col gap-4">
-          <h3 className="text-right text-2xl font-medium">
-            ورود و خروج سرمایه‌گذاران حقیقی به سهام و درآمد ثابت
-          </h3>
-          <div className="flex w-full flex-row justify-between text-sm">
-            <div className="flex flex-row items-center gap-4">
-              <span className="flex flex-row items-center gap-1">
-                <Icon name="layers-2" size="md" />
-                صندوق‌های درآمد ثابت
-              </span>
-              <NewBadge />
-            </div>
-            <div className="flex flex-row items-center gap-3">
-              <div className="flex h-8 w-8 items-center justify-center">
-                <Heart
-                  initialIsliked={false}
-                  onLike={() => console.log('sdasdd')}
-                />
-              </div>
-              <Button align="center" isLoading={false} mode="primary" size="sm">
-                <div className="flex w-fit flex-row gap-2">
-                  <Icon name="plus" size="lg" />
-                  <div> افزودن به داشبورد</div>
-                </div>
-              </Button>
-            </div>
-          </div>
-          <div className="h-0.5 w-full border border-gray-300"></div>
-          <span className="text-md text-right font-medium">معرفی کوتاه:</span>
-          <p className="text-xs font-normal">
-            این گزارش روند ورود و خروج سرمایه‌گذاران حقیقی به دو دسته دارایی را
-            بررسی می‌کند: سهام و درآمد ثابت. به طور خلاصه، میزان
-            سرمایه‌گذاری‌های جدید و برداشت‌های انجام شده در این دو بخش را طی یک
-            دوره زمانی مشخص تحلیل می‌کند تا نوسانات و تمایلات سرمایه‌گذاران
-            حقیقی را نشان دهد.
-          </p>
-        </div>
+        <ReportOverview />
       </div>
     </div>
   );
