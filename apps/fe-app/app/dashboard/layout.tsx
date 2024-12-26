5ba15c57d9673e5470d49621587d732ba373adb7
sinapir2 <sinapir2@gmail.com>
2024-12-26 12:41:09 +0330

    feat: complete the dashboard layput

--- apps/fe-app/app/dashboard/layout.tsx
+++ apps/fe-app/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Footer } from '../(layout)/(footer)';
 import PRODUCT_LOGO from '@aress-assets/icons/product_logo.svg';
+import { HeadProfile, SquaredButton } from 'design-system';
 export default function DashboardLayout({
   children,
 }: {
@@ -23,7 +24,25 @@ export default function DashboardLayout({
               {new Date().toLocaleDateString('fa-IR')}
             </span>
           </div>
-          <div className="flex flex-row gap-3"></div>
+          <div className="flex flex-row gap-3">
+            <HeadProfile
+              name="احمد محمدی"
+              profileImage="https://picsum.photos/200"
+            />
+            <SquaredButton
+              icons={[{ name: 'bell' }]}
+              badge={{ enabled: true, text: '3' }}
+            />
+            <SquaredButton
+              icons={[
+                {
+                  name: 'moon',
+                },
+                { name: 'sun' },
+              ]}
+              badge={{ enabled: false }}
+            />
+          </div>
         </div>
       </div>
       {children}
