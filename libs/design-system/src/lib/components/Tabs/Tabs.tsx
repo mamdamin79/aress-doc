d00a2614e7ee27c2dfa4d2958742a35b43c0ae04
reza <rezasiahi8@gmail.com>
2024-12-26 17:48:08 +0330

    refactor: change key mode to variant and change bgWhite to colorMode

--- libs/design-system/src/lib/components/Tabs/Tabs.tsx
+++ libs/design-system/src/lib/components/Tabs/Tabs.tsx
@@ -5,202 +5,216 @@ import { Icon } from '../Icon';
 
 interface Props {
   tabs: TabItem[];
-  mode: 'shaped' | 'divided' | 'lined' | 'rounded' | 'rounded-full';
-  bgWhite?: boolean;
+  variant: 'shaped' | 'divided' | 'lined' | 'rounded' | 'rounded-full';
+  colorMode: 'neutral' | 'inverse';
   tag?: boolean;
   onClickTab: (idTab: number) => void;
 }
 
 export const Tabs: React.FC<Props> = ({
-  mode,
+  variant,
   tabs,
   onClickTab,
-  bgWhite,
+  colorMode,
   tag,
 }) => {
   return (
     <div className="flex w-full justify-center">
-      <div className="w-full">
-        <TabGroup>
-          <TabList
-            className={cn(
-              'flex',
-              { 'w-max gap-10 border-b-2 border-gray-300': mode === 'lined' },
-              { 'gap-4': mode !== 'lined' && mode !== 'divided' },
-            )}
-          >
-            {tabs.map((props, index) => (
-              <Tab
-                onClick={() => onClickTab(props.id)}
-                key={index}
-                className={cn(
-                  'font-vazirmatn text-md relative outline-none',
-                  {
-                    'data-[selected]:bg-brand-600 relative min-w-40 rounded-t-xl py-2 text-center text-gray-600 hover:text-gray-700 data-[selected]:font-semibold data-[selected]:text-white':
-                      mode === 'lined',
-                  },
-                  {
-                    'data-[selected]:bg-brand-600 data-[selected]:border-brand-600 hover:border-brand-600 border-2 border-white px-4 py-2 font-semibold text-black first:rounded-r-md first:border-r-2 last:rounded-l-md data-[selected]:text-white':
-                      mode === 'divided',
-                  },
-                  {
-                    'bg-gray-100':
-                      !bgWhite && mode !== 'lined' && mode !== 'rounded',
-                  },
-                  {
-                    'data-[selected]:bg-brand-200 data-[selected]:text-brand-800 rounded-sm px-3 text-sm font-medium text-gray-600 hover:bg-gray-100 hover:text-gray-700':
-                      mode === 'rounded',
-                  },
-                  {
-                    'border-2 border-gray-100 bg-white':
-                      mode === 'shaped' && !bgWhite,
-                    'border-2 border-gray-100 bg-gray-100':
-                      mode === 'shaped' && bgWhite,
-                  },
-                  {
-                    'border-white bg-white': mode === 'divided' && !bgWhite,
-                    'border-gray-100 bg-gray-100':
-                      mode === 'divided' && bgWhite,
-                  },
-                  {
-                    'relative hover:bg-white': mode === 'lined' && !bgWhite,
-                  },
-                  {
-                    'relative bg-white hover:bg-gray-100':
-                      mode === 'lined' && bgWhite,
-                  },
-                  {
-                    'data-[selected]:border-brand-600 data-[selected]:bg-brand-600 hover:border-brand-600 rounded-md border-2 border-white px-2 py-1 text-black data-[selected]:text-white':
-                      mode === 'shaped',
-                  },
-                  {
-                    'data-[selected]:bg-brand-600 data-[selected]:border-brand-600 hover:border-brand-600 rounded-full border-2 border-gray-100 px-4 py-2 text-sm font-medium text-black data-[selected]:border-spacing-2 data-[selected]:text-white':
-                      mode === 'rounded-full',
-                  },
-                  {
-                    'bg-gray-100': mode === 'rounded-full' && bgWhite,
-                  },
-                  {
-                    'bg-white': mode === 'rounded-full' && !bgWhite,
-                  },
-                )}
-              >
-                {({ hover, selected }) => (
-                  <>
-                    {mode === 'lined' && (
-                      <>
-                        <div
-                          className={cn(
-                            {
-                              'left-[100%] top-0 z-30 hidden h-full w-4 rounded-bl-3xl':
-                                mode === 'lined',
-                            },
-                            {
-                              'bg-white': mode === 'lined' && bgWhite,
-                            },
-                            {
-                              'bg-gray-100': mode === 'lined' && !bgWhite,
-                            },
-                            { 'absolute block': selected && index },
-                            {
-                              'absolute block': !selected && hover && index,
-                            },
-                          )}
-                        ></div>
-                        <div
-                          className={cn(
-                            'bg-brand-600 left-[100%] top-0 z-10 hidden h-full w-4',
-                            { 'absolute block': selected && index },
-                            {
-                              'absolute block bg-white':
-                                !selected && hover && index && !bgWhite,
-                            },
-                            {
-                              'absolute block bg-gray-100':
-                                !selected && hover && index && bgWhite,
-                            },
-                          )}
-                        ></div>
-                      </>
-                    )}
-                    {mode === 'shaped' ? (
-                      <div className="flex items-center gap-2">
-                        {tag && (
-                          <div className="bg-vividGreen-600 flex h-3 w-3 items-center justify-center rounded-full border-2 border-white"></div>
+      <TabGroup>
+        <TabList
+          className={cn(
+            'flex',
+            { 'w-max gap-10 border-b-2 border-gray-300': variant === 'lined' },
+            { 'gap-4': variant !== 'lined' && variant !== 'divided' },
+          )}
+        >
+          {tabs.map((props, index) => (
+            <Tab
+              onClick={() => onClickTab(+props.id)}
+              key={index}
+              className={cn(
+                'font-vazirmatn text-md relative outline-none',
+                {
+                  'data-[selected]:bg-brand-600 relative min-w-40 rounded-t-xl py-2 text-center text-gray-600 hover:text-gray-700 data-[selected]:font-semibold data-[selected]:text-white':
+                    variant === 'lined',
+                },
+                {
+                  'data-[selected]:bg-brand-600 data-[selected]:border-brand-600 hover:border-brand-600 border-2 border-white px-4 py-2 font-semibold text-black first:rounded-r-md first:border-r-2 last:rounded-l-md data-[selected]:text-white':
+                    variant === 'divided',
+                },
+                {
+                  'bg-gray-100':
+                    colorMode === 'neutral' &&
+                    variant !== 'lined' &&
+                    variant !== 'rounded',
+                },
+                {
+                  'data-[selected]:bg-brand-200 data-[selected]:text-brand-800 rounded-sm px-3 text-sm font-medium text-gray-600 hover:bg-gray-100 hover:text-gray-700':
+                    variant === 'rounded',
+                },
+                {
+                  'border-2 border-gray-100': variant === 'shaped',
+                  'bg-white': variant === 'shaped' && colorMode === 'inverse',
+                  'bg-gray-100':
+                    variant === 'shaped' && colorMode === 'neutral',
+                },
+                {
+                  'border-white bg-white':
+                    variant === 'divided' && colorMode === 'inverse',
+                  'border-gray-100 bg-gray-100':
+                    variant === 'divided' && colorMode === 'neutral',
+                },
+                {
+                  'relative hover:bg-white':
+                    variant === 'lined' && colorMode === 'inverse',
+                },
+                {
+                  'relative bg-white hover:bg-gray-100':
+                    variant === 'lined' && colorMode === 'neutral',
+                },
+                {
+                  'data-[selected]:border-brand-600 data-[selected]:bg-brand-600 hover:border-brand-600 rounded-md border-2 border-white px-2 py-1 text-black data-[selected]:text-white':
+                    variant === 'shaped',
+                },
+                {
+                  'data-[selected]:bg-brand-600 data-[selected]:border-brand-600 hover:border-brand-600 rounded-full border-2 border-gray-100 px-4 py-2 text-sm font-medium text-black data-[selected]:border-spacing-2 data-[selected]:text-white':
+                    variant === 'rounded-full',
+                },
+                {
+                  'bg-gray-100':
+                    variant === 'rounded-full' && colorMode === 'neutral',
+                },
+                {
+                  'bg-white':
+                    variant === 'rounded-full' && colorMode === 'inverse',
+                },
+              )}
+            >
+              {({ hover, selected }) => (
+                <>
+                  {variant === 'lined' && (
+                    <>
+                      <div
+                        className={cn(
+                          {
+                            'left-[100%] top-0 z-30 hidden h-full w-4 rounded-bl-3xl':
+                              variant === 'lined',
+                          },
+                          {
+                            'bg-gray-100':
+                              variant === 'lined' && colorMode === 'inverse',
+                          },
+                          {
+                            'bg-white':
+                              variant === 'lined' && colorMode === 'neutral',
+                          },
+                          { 'absolute block': selected && index },
+                          {
+                            'absolute block': !selected && hover && index,
+                          },
                         )}
-                        {props.icons?.length && (
-                          <Icon {...props.icons[0]} size="lg" />
+                      ></div>
+                      <div
+                        className={cn(
+                          'bg-brand-600 left-[100%] top-0 z-10 hidden h-full w-4',
+                          { 'absolute block': selected && index },
+                          {
+                            'absolute block bg-white':
+                              !selected &&
+                              hover &&
+                              index &&
+                              colorMode === 'inverse',
+                          },
+                          {
+                            'absolute block bg-gray-100':
+                              !selected &&
+                              hover &&
+                              index &&
+                              colorMode === 'neutral',
+                          },
                         )}
-                        {props.title}
-                        {props.icons?.length && props.title && (
-                          <Icon {...props.icons[1]} size="lg" />
+                      ></div>
+                    </>
+                  )}
+                  {variant === 'shaped' ? (
+                    <div className="flex items-center gap-2">
+                      {tag && (
+                        <div className="bg-vividGreen-600 flex h-3 w-3 items-center justify-center rounded-full border-2 border-white"></div>
+                      )}
+                      {props.icons?.length && (
+                        <Icon {...props.icons[0]} size="lg" />
+                      )}
+                      {props.title}
+                      {props.icons?.length && props.title && (
+                        <Icon {...props.icons[1]} size="lg" />
+                      )}
+                    </div>
+                  ) : (
+                    <span>{props.title}</span>
+                  )}
+                  {variant === 'lined' && (
+                    <>
+                      <div
+                        className={cn(
+                          {
+                            'right-[100%] top-0 z-30 hidden h-full w-4 rounded-br-3xl bg-white':
+                              variant === 'lined',
+                          },
+                          {
+                            'bg-gray-100':
+                              variant === 'lined' && colorMode === 'inverse',
+                          },
+                          {
+                            'bg-white':
+                              variant === 'lined' && colorMode === 'neutral',
+                          },
+                          {
+                            'absolute block':
+                              selected && index + 1 < tabs.length,
+                          },
+                          {
+                            'absolute block':
+                              hover && !selected && index + 1 !== tabs.length,
+                          },
                         )}
-                      </div>
-                    ) : (
-                      <span>{props.title}</span>
-                    )}
-                    {mode === 'lined' && (
-                      <>
-                        <div
-                          className={cn(
-                            {
-                              'right-[100%] top-0 z-30 hidden h-full w-4 rounded-br-3xl bg-white':
-                                mode === 'lined',
-                            },
-                            {
-                              'bg-white': mode === 'lined' && bgWhite,
-                            },
-                            {
-                              'bg-gray-100': mode === 'lined' && !bgWhite,
-                            },
-                            {
-                              'absolute block':
-                                selected && index + 1 < tabs.length,
-                            },
-                            {
-                              'absolute block':
-                                hover && !selected && index + 1 !== tabs.length,
-                            },
-                          )}
-                        ></div>
-                        <div
-                          className={cn(
-                            'bg-brand-600 right-[100%] top-0 z-10 hidden h-full w-4',
-                            {
-                              'absolute block':
-                                selected && index + 1 !== tabs.length,
-                            },
+                      ></div>
+                      <div
+                        className={cn(
+                          'bg-brand-600 right-[100%] top-0 z-10 hidden h-full w-4',
+                          {
+                            'absolute block':
+                              selected && index + 1 !== tabs.length,
+                          },
 
-                            {
-                              'absolute block bg-white':
-                                !selected &&
-                                hover &&
-                                index + 1 !== tabs.length &&
-                                !bgWhite,
-                            },
-                            {
-                              'absolute block bg-gray-100':
-                                !selected &&
-                                hover &&
-                                index + 1 !== tabs.length &&
-                                bgWhite,
-                            },
-                          )}
-                        ></div>
-                      </>
-                    )}
-                  </>
-                )}
-              </Tab>
-            ))}
-          </TabList>
-          <TabPanels className="mt-3">
-            {tabs.map(({ content }, index) => (
-              <TabPanel key={index}>{content}</TabPanel>
-            ))}
-          </TabPanels>
-        </TabGroup>
-      </div>
+                          {
+                            'absolute block bg-white':
+                              !selected &&
+                              hover &&
+                              index + 1 !== tabs.length &&
+                              colorMode === 'inverse',
+                          },
+                          {
+                            'absolute block bg-gray-100':
+                              !selected &&
+                              hover &&
+                              index + 1 !== tabs.length &&
+                              colorMode === 'neutral',
+                          },
+                        )}
+                      ></div>
+                    </>
+                  )}
+                </>
+              )}
+            </Tab>
+          ))}
+        </TabList>
+        <TabPanels className="mt-3">
+          {tabs.map(({ content }, index) => (
+            <TabPanel key={index}>{content}</TabPanel>
+          ))}
+        </TabPanels>
+      </TabGroup>
     </div>
   );
 };
