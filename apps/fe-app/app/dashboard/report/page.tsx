66704eab1478aa5d1b198489d8de8133722517ee
sinapir2 <sinapir2@gmail.com>
2024-12-26 17:37:49 +0330

    feat: add ul list to page

--- apps/fe-app/app/dashboard/report/page.tsx
+++ apps/fe-app/app/dashboard/report/page.tsx
@@ -1,13 +1,15 @@
 'use client';
-import { Icon } from 'design-system';
+import { Icon, SectionTitle, Tabs } from 'design-system';
 import Image from 'next/image';
 import React from 'react';
 import fake2 from '../fake2.png';
+import fake1 from '../fake1.png';
 import { ReportOverview } from './_components/ReportOverview';
+import { SectionItem } from './_components/SectionUlItem';
 const page = () => {
   return (
-    <div className="h-[100vh]">
-      <div className="flex w-full flex-row justify-around gap-8 px-20 pt-6">
+    <div>
+      <section className="flex w-full flex-row justify-around gap-8 px-20 pt-6">
         <div className="flex w-fit flex-col gap-2">
           <div className="rounded-3xl bg-gray-100 p-4">
             <Image src={fake2} alt="fake2" width={616} height={320} />
@@ -23,7 +25,63 @@ const page = () => {
           </div>
         </div>
         <ReportOverview />
-      </div>
+      </section>
+      <section className="flex justify-center px-2 pb-14 pt-[72px]">
+        <Tabs
+          mode="rounded-full"
+          onClickTab={(idTab: number) => console.log(idTab)}
+          tabs={[
+            { id: 1, title: 'ویدیو بررسی' },
+            { id: 2, title: 'اطلاعات بیشتر' },
+            { id: 3, title: 'گزارش های مرتبط' },
+          ]}
+        />
+      </section>
+      <section className="flex w-full flex-col items-center">
+        <SectionTitle align="center" level={3} title="ویدیو بررسی" />
+        <div className="mt-12">
+          <Image alt="video" src={fake1} width={816} height={459} />
+        </div>
+      </section>
+      <section className="flex flex-col items-center justify-center pt-[112px]">
+        <SectionTitle align="center" level={3} title="اطلاعات بیشتر" />
+        <div className="mt-12 flex max-w-[960px] flex-col items-center justify-center">
+          <ul className="rtl marker:text-brand-600 list-disc text-xl font-medium marker:text-3xl">
+            {/* Section Component */}
+            <SectionItem
+              title="نرخ بازده تا سررسید (Yield to Maturity - YTM)"
+              paragraphs={[
+                `نرخ بازده تا سررسید یا YTM یکی از مفاهیم مهم در ارزیابی اوراق قرضه است. این نرخ نشان‌دهنده نرخ بازده کلی است که سرمایه‌گذار می‌تواند انتظار داشته باشد اگر اوراق قرضه را تا تاریخ سررسید نگه دارد و تمام پرداخت‌ها (شامل کوپن‌ها و بازپرداخت اصل مبلغ) بر اساس شرایط فعلی به‌موقع پرداخت شوند.`,
+                `YTM معیاری است که به‌طور خلاصه تمام جریان‌های نقدی حاصل از اوراق قرضه (شامل پرداخت‌های دوره‌ای کوپن و بازپرداخت اصل مبلغ در سررسید) را در نظر می‌گیرد و آنها را با قیمت فعلی بازار اوراق قرضه تطبیق می‌دهد تا نرخ بازده کلی را محاسبه کند.`,
+              ]}
+            />
+
+            <SectionItem
+              title="چرا YTM مهم است؟"
+              list={[
+                {
+                  subtitle: 'تصمیم‌گیری سرمایه‌گذاری:',
+                  content: `نرخ بازده تا سررسید یا YTM یکی از مفاهیم مهم در ارزیابی اوراق قرضه است. این نرخ نشان‌دهنده نرخ بازده کلی است که سرمایه‌گذار می‌تواند انتظار داشته باشد اگر اوراق قرضه را تا تاریخ سررسید نگه دارد و تمام پرداخت‌ها به‌موقع پرداخت شوند.`,
+                },
+                {
+                  subtitle: 'مقایسه بازده:',
+                  content: `YTM امکان مقایسه اوراق قرضه با سررسیدهای مختلف با بازده‌های کوپن متفاوت را فراهم می‌آورد.`,
+                },
+              ]}
+            />
+
+            <SectionItem
+              title="نحوه محاسبه YTM"
+              paragraphs={[
+                `محاسبه YTM معمولاً با استفاده از فرمول‌های ریاضی است که ممکن است نیاز به استفاده از نرم‌افزارهای مالی داشته باشد. این محاسبات با جستجوی نرخ‌هایی انجام می‌شود.`,
+              ]}
+            />
+          </ul>
+        </div>
+      </section>
+      <section className="flex flex-col gap-12 pt-[112px]">
+        <SectionTitle align="center" level={2} title="گزارش‌های مرتبط" />
+      </section>
     </div>
   );
 };
