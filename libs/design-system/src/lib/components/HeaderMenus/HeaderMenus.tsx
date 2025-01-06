2d460db1e1e582c77b2e2229a92efd9c3df47b35
sinapir2 <sinapir2@gmail.com>
2025-01-06 16:37:28 +0330

    fix: change gap from 24 to 20

--- libs/design-system/src/lib/components/HeaderMenus/HeaderMenus.tsx
+++ libs/design-system/src/lib/components/HeaderMenus/HeaderMenus.tsx
@@ -1,9 +1,4 @@
-import {
-  Popover,
-  PopoverButton,
-  PopoverPanel,
-  Transition,
-} from '@headlessui/react';
+import { Popover, PopoverButton } from '@headlessui/react';
 import React from 'react';
 import { Icon } from '../Icon';
 import { MenuTiles } from '../MenuTiles';
@@ -18,12 +13,12 @@ interface MenuProps {
 export const HeaderMenus: React.FC<MenuProps> = ({ menuItems }) => {
   const activeTab = 0;
   return (
-    <div className="flex items-center gap-6 text-nowrap">
+    <div className="flex items-center gap-5 text-nowrap">
       {menuItems.map((item, index) => (
-        <Popover key={index} className="relative group h-[40px]">
+        <Popover key={index} className="group relative h-[40px]">
           <PopoverButton
             className={cn(
-              'flex items-center gap-2 py-1 outline-none transition-colors text-shadow-sm font-normal',
+              'text-shadow-sm flex items-center gap-2 py-1 font-normal outline-none transition-colors',
               item.dropdown
                 ? 'group-hover:text-brand-600'
                 : activeTab === index
@@ -36,7 +31,7 @@ export const HeaderMenus: React.FC<MenuProps> = ({ menuItems }) => {
           >
             <div className="relative">
               {item.dropdown && (
-                <span className="absolute block font-medium h-0 overflow-hidden transition-all">
+                <span className="absolute block h-0 overflow-hidden font-medium transition-all">
                   {item.name}
                 </span>
               )}
@@ -46,13 +41,13 @@ export const HeaderMenus: React.FC<MenuProps> = ({ menuItems }) => {
                 <span>{item.name}</span>
               )}
               <div
-                className={`absolute bottom-0 left-0 right-0 mx-auto w-6 h-[6px] bg-brand-600 rounded-full -mb-2 ${
+                className={`bg-brand-600 absolute bottom-0 left-0 right-0 mx-auto -mb-2 h-[6px] w-6 rounded-full ${
                   activeTab === index ? 'group-hover:block' : 'hidden'
                 }`}
               />
             </div>
             {item.dropdown && (
-              <div className="transition-transform duration-200 transform group-hover:rotate-180">
+              <div className="transform transition-transform duration-200 group-hover:rotate-180">
                 <Icon name="chevron-down" size="lg" />
               </div>
             )}
@@ -60,20 +55,20 @@ export const HeaderMenus: React.FC<MenuProps> = ({ menuItems }) => {
 
           {/* Dropdown panel */}
           {item.dropdown && (
-            <div className="flex flex-row absolute shadow-md bg-baseBackground  rounded-xl border-2 border-gray-300 z-10 shadow-offset-y-10">
-              <div className="hidden group-hover:block text-right w-fit max-w-[272px] h-fit py-4 gap-2 flex flex-col">
+            <div className="bg-baseBackground shadow-offset-y-10 absolute z-10 flex flex-row rounded-xl border-2 border-gray-300 shadow-md">
+              <div className="flex hidden h-fit w-fit max-w-[272px] flex-col gap-2 py-4 text-right group-hover:block">
                 {item.dropdown?.map((dropdownItem, dropdownItemIndex) => (
                   <div
                     key={dropdownItemIndex}
                     className={cn(
                       'flex flex-col',
                       dropdownItem.border
-                        ? 'border-t-2 border-b-2 border-gray-200 mb-2'
+                        ? 'mb-2 border-b-2 border-t-2 border-gray-200'
                         : '',
                       dropdownItem.children[0]?.isDashboard ? 'gap-2' : '',
                     )}
                   >
-                    <div className="text-gray-600 text-sm pr-4 font-normal flex flex-row gap-2 items-center">
+                    <div className="flex flex-row items-center gap-2 pr-4 text-sm font-normal text-gray-600">
                       {dropdownItem.groupLabel}
                       {dropdownItem.counter && (
                         <>
@@ -86,7 +81,7 @@ export const HeaderMenus: React.FC<MenuProps> = ({ menuItems }) => {
                       (subItemChildren, subItemChildrenIndex) => (
                         <div
                           key={subItemChildrenIndex}
-                          className="flex relative justify-center"
+                          className="relative flex justify-center"
                         >
                           <MenuTiles
                             {...subItemChildren}
