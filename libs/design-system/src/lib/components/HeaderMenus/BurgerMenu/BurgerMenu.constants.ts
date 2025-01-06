49fb3c48e8294b2d7246afe6e151d848e6a2f139
sinapir2 <sinapir2@gmail.com>
2025-01-06 16:30:17 +0330

    fix: add items to hamburger menu based on the design

--- libs/design-system/src/lib/components/HeaderMenus/BurgerMenu/BurgerMenu.constants.ts
+++ libs/design-system/src/lib/components/HeaderMenus/BurgerMenu/BurgerMenu.constants.ts
@@ -104,19 +104,161 @@ export const menu = [
     groupLabel: '',
     children: [
       {
-        text: 'گزارش ها',
-        expandable: true,
-      },
-      {
         text: 'صندوق‌های سرمایه‌گذاری',
         expandable: true,
       },
       {
         text: 'صندوق من',
         expandable: true,
+        subMenu: [
+          {
+            groupLabel: '',
+
+            children: [
+              {
+                text: 'صندوق سهم آشنا',
+                subText: 'سهامی',
+                link: '#',
+                icon: {
+                  name: 'circle-dot',
+                  size: 'md',
+                },
+                badgeColor: 'bg-green-500',
+              },
+              {
+                text: 'امین آشنا ایرانیان',
+                subText: 'درآمد ثابت',
+                link: '#',
+                icon: {
+                  name: 'circle-dot',
+                  size: 'md',
+                },
+                badgeColor: 'bg-green-500',
+              },
+              {
+                text: 'حکمت آشنا ایرانیان',
+                subText: 'درآمد ثابت',
+                link: '#',
+                icon: {
+                  name: 'circle-dot',
+                  size: 'md',
+                },
+                isActive: true,
+                badgeColor: 'bg-blue-500',
+              },
+            ],
+          },
+          {
+            groupLabel: '',
+            children: [],
+            border: true,
+          },
+          {
+            groupLabel: 'ابزارهای مدیرعامل',
+
+            children: [
+              {
+                text: 'کلیت صندوق‌ها',
+                subText: 'نمای کلی از عملکرد صندوق‌ها',
+                link: '#',
+                icon: {
+                  name: 'clipboard-list',
+                  size: 'md',
+                },
+              },
+              {
+                text: 'ارزیابی مدیرها',
+                subText: 'ارزیابی کارایی و نتایج مدیران',
+                link: '#',
+                icon: {
+                  name: 'circle-dot',
+                  size: 'md',
+                },
+              },
+            ],
+          },
+        ],
       },
       {
         text: 'بازار ها',
+        expandable: true,
+        subMenu: [
+          {
+            groupLabel: '',
+            children: [
+              {
+                text: 'سهام',
+                subText: 'سهام شرکت‌های بورس و فرابورس',
+                icon: {
+                  name: 'cog',
+                  size: 'md',
+                  color: 'text-vividGreen-700',
+                },
+                link: '#',
+              },
+              {
+                text: 'اوراق با درآمد ثابت',
+                subText: 'اوراق بهادار با بازده مشخص و کم ریسک',
+                icon: {
+                  name: 'shield',
+                  size: 'md',
+                  color: 'text-blue-700',
+                },
+                link: '#',
+              },
+              {
+                text: 'اوراق مشتقه',
+                subText: 'قراردادهای مالی وابسته به دارایی های دیگر',
+                icon: {
+                  name: 'network',
+                  size: 'md',
+                  color: 'text-pink-700',
+                },
+                link: '#',
+              },
+              {
+                text: 'طلا و کالای فیزیکی',
+                subText: 'کالاهای استاندارد شده در بورس کالا',
+                icon: {
+                  name: 'landmark',
+                  size: 'md',
+                  color: 'text-yellow-700',
+                },
+                link: '#',
+              },
+              {
+                text: 'املاک و مستغلات',
+                subText: 'بازار املاک برای سرمایه گذاری بلندمدت',
+                icon: {
+                  name: 'building-2',
+                  size: 'md',
+                  color: 'text-gray-700',
+                },
+                link: '#',
+              },
+              {
+                text: 'رمز ارز',
+                subText: 'دارایی های دیجیتال برپایه فناوری بلاک چین',
+                icon: {
+                  name: 'bitcoin',
+                  size: 'md',
+                  color: 'text-red-700',
+                },
+                link: '#',
+              },
+              {
+                text: 'خلاصه کلی',
+                subText: 'بررسی و تحلیل گسترده‌ای از بازارها',
+                icon: {
+                  name: 'chart-pie',
+                  size: 'md',
+                  color: 'text-brand-700',
+                },
+                link: '#',
+              },
+            ],
+          },
+        ],
       },
       {
         text: 'اشتراکات آرسس',
--- libs/design-system/src/lib/components/HeaderMenus/BurgerMenu/BurgerMenu.tsx
+++ libs/design-system/src/lib/components/HeaderMenus/BurgerMenu/BurgerMenu.tsx
@@ -20,26 +20,29 @@ export const BurgerMenu: React.FC<MenuProps> = ({ menuItems }) => {
 
   return (
     <div className="flex items-center gap-6 text-nowrap">
-      <Popover className="relative group h-[40px]">
+      <Popover className="group relative h-[40px]">
         <PopoverButton>
-          <div className="w-8 h-8 flex justify-center items-center">
+          <div className="flex h-8 w-8 items-center justify-center">
             <Icon name="align-justify" size="lg" />
           </div>
         </PopoverButton>
         <div
           onMouseLeave={() => setActiveSubMenu(null)}
-          className="flex flex-row absolute  shadow-8xl bg-baseBackground rounded-xl z-10 shadow-offset-y-10 overflow-hidden"
+          className="shadow-8xl shadow-offset-y-10 absolute z-10 flex flex-row overflow-hidden rounded-xl"
         >
-          <div className="hidden group-hover:block text-right w-fit max-w-[272px] h-fit gap-2 flex flex-col">
+          <div className="bg-baseBackground flex hidden h-fit w-fit max-w-[272px] flex-col gap-2 text-right group-hover:block">
             {menuItems?.map((dropdownItem, dropdownItemIndex) => (
               <div
                 key={dropdownItemIndex}
                 className={cn(
                   'flex flex-col',
+                  dropdownItem.border
+                    ? 'mb-2 border-b border-t border-gray-200'
+                    : '',
                   dropdownItem.children[0]?.isDashboard ? 'gap-2' : '',
                 )}
               >
-                <div className="text-gray-600 text-sm pr-4 font-normal flex flex-row gap-2 items-center">
+                <div className="flex flex-row items-center gap-2 pr-4 text-sm font-normal text-gray-600">
                   {dropdownItem.groupLabel}
                   {dropdownItem.counter && (
                     <>
@@ -52,7 +55,7 @@ export const BurgerMenu: React.FC<MenuProps> = ({ menuItems }) => {
                   (subItemChildren, subItemChildrenIndex) => (
                     <div
                       key={subItemChildrenIndex}
-                      className="flex relative justify-center"
+                      className="relative flex justify-center"
                       onMouseEnter={() =>
                         subItemChildren.subMenu
                           ? setActiveSubMenu(subItemChildren.subMenu)
@@ -70,16 +73,19 @@ export const BurgerMenu: React.FC<MenuProps> = ({ menuItems }) => {
             ))}
           </div>
           {activeSubMenu && (
-            <div className="hidden group-hover:block text-right w-fit max-w-[272px] h-fit py-4 gap-2 flex flex-col bg-baseBackground rounded-xl shadow-md">
+            <div className="invisible flex h-fit w-fit max-w-[272px] flex-col gap-2 rounded-xl py-4 text-right shadow-md group-hover:visible">
               {activeSubMenu?.map((dropdownItem, dropdownItemIndex) => (
                 <div
                   key={dropdownItemIndex}
                   className={cn(
                     'flex flex-col',
+                    dropdownItem.border
+                      ? 'mb-2 border-b border-t border-gray-200'
+                      : '',
                     dropdownItem.children[0]?.isDashboard ? 'gap-2' : '',
                   )}
                 >
-                  <div className="text-gray-600 text-sm pr-4 font-normal flex flex-row gap-2 items-center">
+                  <div className="bg-baseBackground flex flex-row items-center gap-2 pr-4 text-sm font-normal text-gray-600">
                     {dropdownItem.groupLabel}
                     {dropdownItem.counter && (
                       <>
@@ -92,7 +98,7 @@ export const BurgerMenu: React.FC<MenuProps> = ({ menuItems }) => {
                     (subItemChildren, subItemChildrenIndex) => (
                       <div
                         key={subItemChildrenIndex}
-                        className="flex relative justify-center"
+                        className="relative flex justify-center"
                       >
                         <MenuTiles
                           {...subItemChildren}
