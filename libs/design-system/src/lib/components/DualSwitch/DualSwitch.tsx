c7778ed8a3096d9a8c26aac0ddb0212c98ab2f67
sinapir2 <sinapir2@gmail.com>
2025-01-09 16:26:38 +0330

    fix: remove border

--- libs/design-system/src/lib/components/DualSwitch/DualSwitch.tsx
+++ libs/design-system/src/lib/components/DualSwitch/DualSwitch.tsx
@@ -53,12 +53,12 @@ export const DualSwitch: React.FC<DualSwitchProps> = ({
   return (
     <div
       className={cn(
-        'rounded-[100px] h-fit w-fit border p-1 gap-1 flex flex-row shadow-xs text-xs',
+        'shadow-xs flex h-fit w-fit flex-row gap-1 rounded-[100px] p-1 text-xs',
         bgWhite ? 'bg-white' : 'bg-gray-100 transition-colors',
         size === 'lg'
           ? disabled
-            ? 'border-brand-300'
-            : 'border-brand-600'
+            ? 'border-brand-300 border'
+            : 'border-brand-600 border'
           : '',
       )}
     >
