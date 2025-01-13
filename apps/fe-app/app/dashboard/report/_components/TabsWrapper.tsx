0b2456d572adc1946e1d9f812265812f809c509d
sinapir2 <sinapir2@gmail.com>
2025-01-13 16:54:25 +0330

    fix: refactor tabs wrapper

--- apps/fe-app/app/dashboard/report/_components/TabsWrapper.tsx
+++ apps/fe-app/app/dashboard/report/_components/TabsWrapper.tsx
@@ -1,31 +1,32 @@
 'use client';
-import { useHeaderVisibility } from 'apps/fe-app/hooks';
-import { cn, Tabs } from 'design-system';
+
 import React from 'react';
+import { cn, Tabs } from 'design-system';
+
+const TABS = [
+  { id: 'videoReview', title: 'ویدیو بررسی' },
+  { id: 'moreInfo', title: 'اطلاعات بیشتر' },
+  { id: 'relatedReports', title: 'گزارش های مرتبط' },
+];
 
 export const TabsWrapper: React.FC = () => {
-  const { contentStart, isHeaderVisible } = useHeaderVisibility();
-  const scroll = (id: string) => {
-    const section = document.querySelector('#' + id);
-    console.log(id);
-    section?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   };
+
   return (
     <section
-      className={cn('sticky z-40 mb-8 bg-white px-2 pb-6 [&>div]:pt-6')}
-      style={{
-        top: contentStart - 10,
-      }}
+      className={cn('sticky z-30 mb-6 bg-white px-2 pt-3')}
+      style={{ top: 0 }}
     >
       <Tabs
         variant="rounded-full"
         colorMode="neutral"
-        onClickTab={(id: string) => scroll(id)}
-        tabs={[
-          { id: 'videoReview', title: 'ویدیو بررسی' },
-          { id: 'moreInfo', title: 'اطلاعات بیشتر' },
-          { id: 'relatedReports', title: 'گزارش های مرتبط' },
-        ]}
+        onClickTab={scrollToSection}
+        tabs={TABS}
       />
     </section>
   );
