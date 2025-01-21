35cf3ded40b1aad02d13eab2d02fa69df5f3a306
reza <rezasiahi8@gmail.com>
2025-01-21 10:30:39 +0330

    feat: add test for button component

--- libs/design-system/src/lib/components/Button/Button.test.tsx
+++ libs/design-system/src/lib/components/Button/Button.test.tsx
@@ -2,7 +2,6 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { Button } from './Button';
 import { describe } from 'node:test';
 import '@testing-library/jest-dom';
-import { ButtonMode } from './Button.types';
 
 describe('Button component', () => {
   const handleClick = jest.fn();
@@ -131,100 +130,4 @@ describe('Button component', () => {
     const rightText = rightContainer.querySelectorAll('.text-right');
     expect(rightText.length).toBeGreaterThan(0);
   });
-
-  test.each<[ButtonMode, string]>([
-    ['primary', 'bg-brand-600 active:bg-brand-800'],
-    ['secondary', 'active:bg-brand-800'],
-    ['text', 'active:text-brand-800 active:border-brand-800'],
-    ['underline', 'active:text-brand-800 active:border-brand-800'],
-  ])('checks active state style for %s button', (mode, expectedClass) => {
-    render(
-      <Button
-        mode={mode}
-        size="md"
-        align="center"
-        isLoading={false}
-        disabled={false}
-      >
-        Click me
-      </Button>,
-    );
-
-    const button = screen.getByRole('button');
-    fireEvent.click(button);
-    expect(button).toHaveClass(expectedClass);
-  });
-
-  test.each<[ButtonMode, string]>([
-    ['primary', 'hover:bg-brand-700'],
-    ['secondary', 'hover:bg-brand-700 hover:text-white'],
-    ['text', 'hover:border-brand-600'],
-  ])('checks hover state style for %s button', (mode, expectedClass) => {
-    render(
-      <Button
-        mode={mode}
-        size="md"
-        align="center"
-        isLoading={false}
-        disabled={false}
-      >
-        Click me
-      </Button>,
-    );
-
-    const button = screen.getByRole('button');
-    fireEvent.mouseOver(button);
-    expect(button).toHaveClass(expectedClass);
-  });
-
-  test.each<[ButtonMode, string]>([
-    ['primary', 'bg-brand-600'],
-    ['secondary', 'border-brand-600 text-brand-600'],
-    ['text', 'text-brand-600'],
-    ['underline', 'text-brand-600'],
-  ])('checks loading state for %s button', (mode, expectedClass) => {
-    const { container } = render(
-      <Button
-        mode={mode}
-        size="md"
-        align="center"
-        isLoading={true}
-        disabled={false}
-      >
-        Click me
-      </Button>,
-    );
-
-    const spinnerElements = container.querySelectorAll('.animate-spin');
-    expect(spinnerElements.length).toBeGreaterThan(0);
-
-    const button = screen.getByRole('button');
-    expect(button).toHaveClass(expectedClass);
-  });
-
-  test.each<[ButtonMode, string, string]>([
-    ['primary', 'bg-brand-600 text-white', 'h-12 rounded-lg'],
-    ['secondary', 'border-brand-600 bg-white', 'h-12 rounded-lg'],
-    ['text', 'text-brand-600', 'h-12 rounded-lg'],
-    ['underline', 'text-brand-600', 'h-12 rounded-lg'],
-  ])(
-    'checks correct styles for %s mode',
-    (mode, expectedClass, expectedSizeClass) => {
-      render(
-        <Button
-          mode={mode}
-          size="md"
-          align="center"
-          isLoading={false}
-          disabled={false}
-        >
-          Click me
-        </Button>,
-      );
-
-      const button = screen.getByRole('button');
-      expect(button).toHaveClass(expectedClass);
-      expect(button).toHaveClass(expectedSizeClass);
-    },
-  );
 });
