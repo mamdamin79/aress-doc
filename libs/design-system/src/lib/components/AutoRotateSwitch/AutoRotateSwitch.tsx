0630096078013825a4b7d107012cc1e1c2e24416
sinapir2 <sinapir2@gmail.com>
2025-01-26 11:50:43 +0330

    fix: fix auto rotate switch

--- libs/design-system/src/lib/components/AutoRotateSwitch/AutoRotateSwitch.tsx
+++ libs/design-system/src/lib/components/AutoRotateSwitch/AutoRotateSwitch.tsx
@@ -3,8 +3,6 @@ import { Icon } from '../Icon';
 import { cn } from 'libs/design-system/src/utils';
 import { AutoRotateProps } from './AutoRotateSwitch.types';
 
-
-
 export const AutoRotateSwitch: React.FC<AutoRotateProps> = ({
   rotateOptions,
   initialValue,
@@ -17,7 +15,7 @@ export const AutoRotateSwitch: React.FC<AutoRotateProps> = ({
   const [isOpen, setIsOpen] = useState(false);
 
   const handleMouseEnter = () => {
-    if (!disabled) setIsOpen(true);
+    if (!disabled && !activeRotateOption) setIsOpen(true);
   };
 
   const handleMouseLeave = () => {
@@ -46,14 +44,23 @@ export const AutoRotateSwitch: React.FC<AutoRotateProps> = ({
 
   return (
     <div
-      className="group flex w-10 flex-col gap-1 rounded-full"
+      className="group flex h-10 flex-row gap-1 rounded-full"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <div
         className={cn(
-          'border-brand-600 flex w-10 flex-col items-center justify-center gap-1 rounded-full border py-[6px] transition-all',
-          activeRotateOption ? 'bg-brand-600 p-1 text-white' : 'h-10',
+          'invisible flex h-full -translate-x-7 select-none flex-row-reverse items-center justify-center gap-1 rounded-full border border-gray-400 p-1 font-medium opacity-0 transition-all duration-300',
+          isOpen &&
+            'group-hover:visible group-hover:translate-x-0 group-hover:opacity-100',
+        )}
+      >
+        {rotateOptions.map(renderOption)}
+      </div>
+      <div
+        className={cn(
+          'border-brand-600 flex h-10 flex-row items-center justify-center gap-1 rounded-full border px-[6px] transition-all',
+          activeRotateOption ? 'bg-brand-600 p-1 text-white' : 'w-10',
           isOpen && activeRotateOption && 'border-red-600 bg-red-600',
         )}
       >
@@ -62,12 +69,9 @@ export const AutoRotateSwitch: React.FC<AutoRotateProps> = ({
             'flex h-8 w-8 items-center justify-center transition-all duration-300',
             !activeRotateOption && 'group-hover:rotate-90',
           )}
-          onClick={() => activeRotateOption && changeActiveOption(null)}
         >
-          {!(activeRotateOption && isOpen) ? (
+          {!(activeRotateOption && isOpen) && (
             <Icon name="refresh-cw" size="md" />
-          ) : (
-            <Icon name="power" size="md" />
           )}
         </div>
         {activeRotateOption && !disabled && !isOpen && (
@@ -75,15 +79,6 @@ export const AutoRotateSwitch: React.FC<AutoRotateProps> = ({
             {activeRotateOption}s
           </div>
         )}
-      </div>
-      <div
-        className={cn(
-          'invisible flex w-full -translate-y-7 select-none flex-col items-center justify-center gap-1 rounded-full border border-gray-400 p-1 font-medium opacity-0 transition-all duration-300',
-          isOpen &&
-            'group-hover:visible group-hover:translate-y-0 group-hover:opacity-100',
-        )}
-      >
-        {rotateOptions.map(renderOption)}
       </div>
     </div>
   );
