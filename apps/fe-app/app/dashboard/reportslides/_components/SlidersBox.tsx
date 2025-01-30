7a5dbbc2b190257142e3a690fd9e521ddb2c63d5
sinapir2 <sinapir2@gmail.com>
2025-01-30 12:48:49 +0330

    fix: temp

--- apps/fe-app/app/dashboard/reportslides/_components/SlidersBox.tsx
+++ apps/fe-app/app/dashboard/reportslides/_components/SlidersBox.tsx
@@ -1,28 +1,29 @@
 'use client';
 import { AddReportButton, Icon } from 'design-system';
 import React from 'react';
+
 export const SlidersBox: React.FC = () => {
   return (
     <div className="grid w-[616px] grid-cols-1 content-center gap-6 xl:w-[1256px] xl:grid-cols-2">
-      <div className="shadow-6xl w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
+      <div className="shadow-6xl h-[340px] w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
         <AddReportButton onClick={() => {}} />
       </div>
-      <div className="shadow-6xl w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
+      <div className="shadow-6xl h-[340px] w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
         <AddReportButton onClick={() => {}} />
       </div>
-      <div className="shadow-6xl w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
+      <div className="shadow-6xl h-[340px] w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
         <AddReportButton onClick={() => {}} />
       </div>
-      <div className="shadow-6xl w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
+      <div className="shadow-6xl h-[340px] w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
         <AddReportButton onClick={() => {}} />
       </div>
-      <div className="shadow-6xl w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
+      <div className="shadow-6xl h-[340px] w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
         <AddReportButton onClick={() => {}} />
       </div>
-      <div className="shadow-6xl w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
+      <div className="shadow-6xl h-[340px] w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
         <AddReportButton onClick={() => {}} />
       </div>
-      <div className="shadow-6xl w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
+      <div className="shadow-6xl h-[340px] w-[616px] overflow-hidden rounded-2xl border-2 border-gray-200">
         <AddReportButton onClick={() => {}} />
       </div>
     </div>
--- apps/fe-app/app/dashboard/reportslides/page.tsx
+++ apps/fe-app/app/dashboard/reportslides/page.tsx
@@ -2,28 +2,20 @@ import React from 'react';
 import { DashboardNumberAndName, SlidersBox } from './_components';
 import { AutoRotateSwitchWrapper } from './_components/AutoRotateSwitchWrapper';
 import { FundsSidebarWrapper } from './_components/FundsSidebarWrapper';
-import { HorizontalScrollBar } from 'design-system';
-import { HorizontallScrollWrapper } from './_components/HorizontallScrollWrapper';
 
 const page = () => {
   return (
-    <div className="3xl:px-6 relative mt-8 flex min-h-screen w-full flex-row justify-center gap-20 lg:px-4 xl:px-0">
-      <div className="3xl:block hidden h-fit lg:block xl:hidden">
+    <div className="3xl:px-6 relative mt-8 flex w-full flex-row justify-center gap-20 lg:px-4 xl:px-0">
+      <div className="3xl:block hidden lg:block xl:hidden">
         <FundsSidebarWrapper />
       </div>
-      <div className="relative w-fit">
-        <div className="fixed right-4 top-1/2 z-10 lg:hidden">
-          <HorizontallScrollWrapper />
-        </div>
+      <div className="w-fit">
         <div className="flex w-full justify-between">
           <DashboardNumberAndName number={2} title="صندوق کالایی" />
           <AutoRotateSwitchWrapper />
         </div>
 
-        <section className="relative mt-8 flex w-fit justify-center">
-          <div className="absolute -right-10 top-64 z-10 hidden lg:block">
-            <HorizontallScrollWrapper />
-          </div>
+        <section className="mt-8 flex w-fit justify-center">
           <SlidersBox />
         </section>
       </div>
