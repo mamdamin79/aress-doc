b0f1d970d05398e42be0ea1bdd24fa8ccd4cc5d7
reza <rezasiahi8@gmail.com>
2025-01-30 13:01:29 +0330

    feat: create funds filter section components

---
+++ libs/design-system/src/lib/components/FundsFilterSection/FundsFilterSection.tsx
@@ -0,0 +1,39 @@
+import { Checkbox } from '../Checkbox';
+
+interface SelectedColumnsType {
+  [key: string]: boolean;
+}
+
+interface Props {
+  title: string;
+  options: string[];
+  selectedColumns: SelectedColumnsType;
+  onToggle: (section: string, option: string) => void;
+}
+
+export const FundsFilterSection: React.FC<Props> = ({
+  title,
+  options,
+  selectedColumns,
+  onToggle,
+}) => {
+  return (
+    <div className="mb-2 rounded-md border p-2">
+      <span className="text-base font-semibold">{title}:</span>
+      <div className="mt-4 grid grid-cols-2 gap-3">
+        {options.map((option) => (
+          <div
+            key={option}
+            className="hover:bg-brand-200 flex w-full cursor-pointer items-center gap-2 rounded-md px-2 py-2"
+          >
+            <Checkbox
+              checked={!!selectedColumns[option]}
+              onChange={() => onToggle(title, option)}
+              content={option}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
