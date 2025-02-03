c0238ecda611f7cab814e4e612f43d53ea6dd3c7
sinapir2 <sinapir2@gmail.com>
2025-02-03 08:59:36 +0330

    feat: add types for page

---
+++ apps/fe-app/app/dashboard/report/_types/api.types.ts
@@ -0,0 +1,39 @@
+interface Category {
+  title: string;
+  identifier: number;
+}
+
+interface Video {
+  identifier: number;
+  mp4Video1080P: string;
+  mp4Video1080PSizeBytes: number;
+  mp4Video720P: string;
+  mp4Video720PSizeBytes: number;
+  mp4Video480P: string;
+  mp4Video480PSizeBytes: number;
+  mp4Video360P: string;
+  mp4Video360PSizeBytes: number;
+  mp4Video240P: string;
+  mp4Video240PSizeBytes: number;
+  durationInSeconds: number;
+  poster1080P: string;
+  poster720P: string;
+  poster480P: string;
+  poster360P: string;
+  poster240P: string;
+  thumbnailImage: string;
+}
+
+interface ReportDetailPageApiResponse {
+  identifier: number;
+  title: string;
+  htmlDescription: string;
+  category: Category;
+  image: string;
+  summary: string;
+  video: Video;
+  userFavorite: boolean;
+  isNew: boolean;
+  relatedReports: Report[];
+}
+
