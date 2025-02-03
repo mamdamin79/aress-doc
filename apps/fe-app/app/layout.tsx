06d50e1fd1ba51899a73deebb44eb3a782e40ace
mamdamin79 <mohammadaminsaheb@gmail.com>
2025-02-03 17:00:42 +0330

    chore: add react query

--- apps/fe-app/app/layout.tsx
+++ apps/fe-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import { FooterLite } from './(layout)/(footer-lite)';
 import './global.css';
 import { cn, Header } from 'design-system';
+import ReactQueryProvider from './providers/ReactQueryProvider';
 
 export const metadata = {
   title: 'Welcome to apps/fe-app',
@@ -14,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <ReactQueryProvider>
+        <body>{children}</body>
+      </ReactQueryProvider>
     </html>
   );
 }
