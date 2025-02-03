fc581fa74aa4ed16265a585bc72bb39631bce806
sinapir2 <sinapir2@gmail.com>
2025-02-03 08:41:08 +0330

    fix: border

--- libs/design-system/src/lib/components/AddReportButton/AddReportButton.tsx
+++ libs/design-system/src/lib/components/AddReportButton/AddReportButton.tsx
@@ -12,7 +12,7 @@ export const AddReportButton: React.FC<AddReportButtonProps> = ({
       onClick={onClick}
     >
       <div className="flex flex-col gap-4 justify-center items-center transition-all">
-        <div className="w-[72px] h-[72px] rounded-full border-2 border-dashed border-gray-500 group-hover/reportBtn:border-brand-700 flex justify-center items-center">
+        <div className="w-[72px] h-[72px] rounded-full border-2 border-dashed border-gray-500 group-hover/reportBtn:border-brand-700  transition-all group-hover/reportBtn:border-solid flex justify-center items-center">
           <Icon name="plus" size="lg" />
         </div>
         <span className="text-center">افزودن گزارش جدید</span>
