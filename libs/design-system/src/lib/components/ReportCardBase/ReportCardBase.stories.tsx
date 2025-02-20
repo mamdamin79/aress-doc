0d8080e072f24aa936611db84992f3e9b6a77f53
sinapir2 <sinapir2@gmail.com>
2025-02-20 15:33:33 +0330

    feat: add new story

--- libs/design-system/src/lib/components/ReportCardBase/ReportCardBase.stories.tsx
+++ libs/design-system/src/lib/components/ReportCardBase/ReportCardBase.stories.tsx
@@ -14,11 +14,131 @@ export default meta;
 type Story = StoryObj<typeof ReportCardBase>;
 
 // A default story for the ReportCardBase component
-export const Default: Story = {
+export const showAllOptions: Story = {
   render: (args) => {
     return <ReportCardBase {...args} />;
   },
   args: {
+    title: 'تایتل (اسم گزارش)',
+    switchIcons: {
+      items: [
+        {
+          icon: { name: 'grid-3x3' },
+        },
+        {
+          icon: { name: 'chart-scatter' },
+        },
+      ],
+      onChange(value: number) {
+        console.log(value);
+      },
+      size: 'sm',
+      bgWhite: false,
+      initialIndex: 1,
+    },
+    optionsListItems: {
+      categories: [
+        {
+          id: 1,
+          title: 'همه',
+        },
+        {
+          id: 2,
+          title: 'سهامی',
+        },
+        {
+          id: 3,
+          title: 'درآمد ثابت',
+        },
+        {
+          id: 4,
+          title: 'کالایی',
+        },
+        {
+          id: 5,
+          title: 'مختلط',
+        },
+        {
+          id: 6,
+          title: 'دیده بان',
+        },
+      ],
+      items: [
+        {
+          title: 'ذغال سنگ',
+          categoryId: 2,
+          priceRials: 11250,
+          type: 'سهامی',
+        },
+        {
+          title: 'مس',
+        },
+        {
+          title: 'آلومینیوم',
+          categoryId: 5,
+          priceChangePercent: -1.1,
+          priceRials: 30000,
+          type: 'مختلط',
+        },
+        {
+          title: 'پتروشیمی',
+          categoryId: 6,
+          priceChangePercent: 3.5,
+          priceRials: 22000,
+          type: 'دیده بان',
+        },
+        {
+          title: 'معدنی',
+          categoryId: 2,
+          priceChangePercent: 0.5,
+          priceRials: 18000,
+          type: 'سهامی',
+        },
+        {
+          title: 'گاز',
+          categoryId: 3,
+          priceChangePercent: -2.5,
+          priceRials: 35000,
+          type: 'درآمد ثابت',
+        },
+        {
+          title: 'برق',
+          categoryId: 3,
+          priceChangePercent: 4.0,
+          priceRials: 15000,
+          type: 'درآمد ثابت',
+        },
+        {
+          title: 'کاشی',
+          categoryId: 3,
+          priceChangePercent: -1.5,
+          priceRials: 27000,
+          type: 'درآمد ثابت',
+        },
+        {
+          title: 'شیمیایی',
+          categoryId: 4,
+          priceChangePercent: 5.0,
+          priceRials: 33000,
+          type: 'کالایی',
+        },
+        {
+          title: 'برق',
+          categoryId: 3,
+          priceChangePercent: 4.0,
+          priceRials: 15000,
+          type: 'درآمد ثابت',
+        },
+      ],
+    },
+  },
+};
+export const showSettingsOnly: Story = {
+  render: (args) => {
+    return <ReportCardBase {...args} />;
+  },
+  args: {
+    showSettingsOnly: true,
     title: 'تایتل (اسم گزارش)',
     switchIcons: {
       items: [
