3ab212c2e6ae6158de248bea94f8bd6c9af6c43d
sinapir2 <sinapir2@gmail.com>
2025-02-23 11:51:10 +0330

    feat: preview image

--- libs/design-system/src/lib/components/ProfileImageAndUpload/ProfileImageAndUpload.tsx
+++ libs/design-system/src/lib/components/ProfileImageAndUpload/ProfileImageAndUpload.tsx
@@ -2,20 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Icon } from '../Icon';
 import { FileUploader } from 'react-drag-drop-files';
 import { FileUploadErrorType } from '../FileUpload/FileUpload.constants';
-import { formatFileSize } from '../FileUpload/FileUpload.utils';
+import USER_SVG from '../../../assets/icons/profile vector-large.svg';
 import Image from 'next/image';
-const UserSVG = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="120"
-    height="120"
-    viewBox="0 0 24 24"
-    fill="#CACDD3"
-  >
-    <circle cx="12" cy="8" r="5" />
-    <path d="M20 21a8 8 0 0 0-16 0" />
-  </svg>
-);
+import { cn } from 'libs/design-system/src/utils';
+
 type FileUploadProps = {
   types: string[];
   maxSize: number;
@@ -30,19 +20,26 @@ export const ProfileImageAndUpload: React.FC<FileUploadProps> = ({
   image,
 }) => {
   const [file, setFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(image || null);
 
   useEffect(() => {
     if (!Array.isArray(types) || types.length === 0) {
       onError?.('INVALID_FILE_TYPE');
       return;
-    } else {
-      return;
     }
   }, [types, onError]);
 
+  useEffect(() => {
+    if (file) {
+      const objectUrl = URL.createObjectURL(file);
+      setPreview(objectUrl);
+      return () => URL.revokeObjectURL(objectUrl);
+    }
+  }, [file]);
+
   const handleFileChange = (selectedFile: File) => {
-    const fileExtension = selectedFile.name.split('.').pop();
-    if (!!fileExtension && !types.includes(fileExtension)) {
+    const fileExtension = selectedFile.name.split('.').pop()?.toLowerCase();
+    if (!fileExtension || !types.includes(fileExtension)) {
       onError?.('INVALID_FILE_TYPE');
       return;
     }
@@ -51,32 +48,26 @@ export const ProfileImageAndUpload: React.FC<FileUploadProps> = ({
       onError?.('FILE_TOO_LARGE');
       return;
     }
-    console.log('valid');
+
     setFile(selectedFile);
   };
-
+  const imageDimension = preview ? 120 : 80;
   return (
-    <>
-      <FileUploader handleChange={handleFileChange} name="file" types={types}>
-        <div className="bg-baseBackground relative h-32 w-32 rounded-full border-2 border-gray-100 p-1">
-          <div className="flex h-full w-full items-center justify-center overflow-hidden rounded-full">
-            {image ? (
-              <Image
-                alt="profile image"
-                src={image}
-                width={120}
-                height={120}
-                className="h-[120px] w-[120px] object-contain"
-              />
-            ) : (
-              <UserSVG />
-            )}
-          </div>
-          <div className="bg-baseBackground absolute left-[88px] top-[88px] flex h-10 w-10 items-center justify-center rounded-full text-gray-700 shadow-2xl">
-            <Icon name="image-up" size="lg" />
-          </div>
+    <FileUploader handleChange={handleFileChange} name="file" types={types}>
+      <div className="bg-baseBackground relative h-32 w-32 rounded-full border-2 border-gray-100 p-1">
+        <div className="flex h-full w-full items-end justify-center overflow-hidden rounded-full">
+          <Image
+            alt="profile image"
+            src={preview ? preview : USER_SVG}
+            width={imageDimension}
+            height={imageDimension}
+            className={cn('object-cover', preview && `h-[120px] w-[120px]`)}
+          />
         </div>
-      </FileUploader>
-    </>
+        <div className="bg-baseBackground absolute left-[88px] top-[88px] flex h-10 w-10 items-center justify-center rounded-full text-gray-700 shadow-2xl">
+          <Icon name="image-up" size="lg" />
+        </div>
+      </div>
+    </FileUploader>
   );
 };
