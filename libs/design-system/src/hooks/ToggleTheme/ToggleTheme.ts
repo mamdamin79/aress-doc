46d3e04a045517b1953bc32212840bae1f6808e9
sinapir2 <sinapir2@gmail.com>
2025-03-09 15:58:55 +0330

    feat: add hook for toggling color theme

---
+++ libs/design-system/src/hooks/ToggleTheme/ToggleTheme.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const useThemeToggle = (): {
+  theme: 'light' | 'dark';
+  toggleTheme: () => void;
+} => {
+  const [theme, setTheme] = useState<'light' | 'dark'>(() => {
+    if (typeof window !== 'undefined') {
+      return (localStorage.getItem('theme') as 'light' | 'dark') || 'light';
+    }
+    return 'light';
+  });
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = (): void => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return { theme, toggleTheme };
+};
+
+export default useThemeToggle;
