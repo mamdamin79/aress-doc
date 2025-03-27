6725262716f60be8bd084bcd77cff3a3d8358ad5
sinapir2 <sinapir2@gmail.com>
2025-03-27 12:07:37 +0330

    fix: revert changes on icon

--- libs/design-system/src/lib/components/Icon/Icon.constants.ts
+++ libs/design-system/src/lib/components/Icon/Icon.constants.ts
@@ -6,7 +6,6 @@ export const SIZE_VALUES: Record<IconSize, number> = {
   md: 20,
   lg: 24,
   lg_plus: 32,
-  lg_plus_plus: 40,
   xl: 48,
 };
 
@@ -15,6 +14,5 @@ export const STROKE_VALUES: Record<IconSize, number> = {
   md: 1.5,
   lg: 1.8,
   lg_plus: 2,
-  lg_plus_plus: 3,
   xl: 5.5,
 };
