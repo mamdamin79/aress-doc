ec596a57e9b893803bc668d616bca140265440d6
sinapir2 <sinapir2@gmail.com>
2025-03-27 13:43:52 +0330

    fix: fix storybook

--- libs/design-system/src/lib/components/HeadProfile/HeadProfile.stories.tsx
+++ libs/design-system/src/lib/components/HeadProfile/HeadProfile.stories.tsx
@@ -1,6 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { HeadProfile } from './HeadProfile';
-import { Icon } from '../Icon';
 const meta: Meta<typeof HeadProfile> = {
   component: HeadProfile,
 };
@@ -11,7 +10,6 @@ type Story = StoryObj<typeof HeadProfile>;
 
 export const Default: Story = {
   args: {
-    name: 'سینا پیروزمندان',
     profileImage: 'https://picsum.photos/200',
   },
 };
