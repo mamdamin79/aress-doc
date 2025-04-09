d4b91f507f82953cbd2e5ca280816a0f34cadaab
sinapir2 <sinapir2@gmail.com>
2025-04-09 16:21:10 +0330

    fix: fix storybook

--- libs/design-system/src/lib/components/PopupInfo/PopupInfo.stories.tsx
+++ libs/design-system/src/lib/components/PopupInfo/PopupInfo.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { PopupInfo } from './PopupInfo';
 import React, { useState } from 'react';
+
 const meta: Meta<typeof PopupInfo> = {
   component: PopupInfo,
 };
@@ -9,36 +10,38 @@ export default meta;
 
 type Story = StoryObj<typeof PopupInfo>;
 
+const PopupInfoStory = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)}>Open dialog</button>
+      <PopupInfo
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        itemsList={[
+          {
+            title: '1. نمادهای مالی',
+            content: 'k: هزار ریال - M:میلیون ریال - B:میلیارد ریال',
+            link: '/',
+          },
+          {
+            title: 'ارزش کل خرید حقیقی',
+            content:
+              'مجموع ارزش معاملاتی که توسط سرمایه‌گذاران حقیقی در بازار بورس انجام شده',
+          },
+          {
+            title: 'ارزش کل خرید حقوقی',
+            content:
+              'مجموع ارزش معاملاتی که توسط سرمایه‌گذاران حقوقی (شرکت‌ها، سازمان‌ها و نهادهای مالی) در بازار بورس انجام شده است.',
+          },
+        ]}
+        title="تعاریف مالی به کار رفته."
+      />
+    </div>
+  );
+};
+
 export const Default: Story = {
-  render: () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-      <div>
-        <button onClick={() => setIsOpen(true)}>Open dialog</button>
-        <PopupInfo
-          isOpen={isOpen}
-          onClose={() => setIsOpen(false)}
-          itemsList={[
-            {
-              title: '1. نمادهای مالی',
-              content: 'k: هزار ریال - M:میلیون ریال - B:میلیارد ریال',
-              link: '/',
-            },
-            {
-              title: 'ارزش کل خرید حقیقی',
-              content:
-                'مجموع ارزش معاملاتی که توسط سرمایه‌گذاران حقیقی در بازار بورس انجام شده',
-            },
-            {
-              title: 'ارزش کل خرید حقوقی',
-              content:
-                'مجموع ارزش معاملاتی که توسط سرمایه‌گذاران حقوقی (شرکت‌ها، سازمان‌ها و نهادهای مالی) در بازار بورس انجام شده است.',
-            },
-          ]}
-          title="تعاریف مالی به کار رفته."
-        />
-      </div>
-    );
-  },
+  render: () => <PopupInfoStory />,
   args: {},
 };
