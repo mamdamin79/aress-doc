5fd43e2373307ef0fc1cce6344fe6858d0b4a496
reza <rezasiahi8@gmail.com>
2025-04-09 14:18:15 +0330

    feat: add gap between sections in LiteFooter

--- apps/fe-app/app/(layout)/(footer-lite)/FooterLite.tsx
+++ apps/fe-app/app/(layout)/(footer-lite)/FooterLite.tsx
@@ -13,11 +13,11 @@ export function FooterLite() {
         <Link href="/">
           <Image width={251} height={72} src={Logo} className="h-[72px] w-[251px]" alt="logo" />
         </Link>
-        <div className="mt-8 grid grid-cols-4 lg:grid-cols-5 grid-rows-4 md:grid-rows-2 lg:grid-rows-1 items-start">
+        <div className="mt-8 grid grid-cols-4 gap-4 lg:grid-cols-5 grid-rows-4 md:grid-rows-2 lg:grid-rows-1 items-start">
           <Link className="col-span-1 row-start-1 text-3xl font-medium" href="/">
             ارتباط با ما
           </Link>
-          <div className="col-span-4 md:col-span-1 row-start-2 md:row-start-2 lg:row-start-1 flex flex-col gap-2">
+          <div className="col-span-4 h-fit md:col-span-1 row-start-2 md:row-start-2 lg:row-start-1 flex flex-col gap-2">
             <span className="text-gray-1000 text-lg">شبکه های اجتماعی:‌</span>
             <div className="flex items-center gap-6">
               <Link href='/'>
@@ -49,11 +49,11 @@ export function FooterLite() {
               </Link>
             </div>
           </div>
-          <div className="col-span-4 md:col-span-1 row-start-3 md:row-start-2 lg:row-start-1 flex flex-col gap-2">
+          <div className="col-span-4 h-fit md:col-span-1 row-start-3 md:row-start-2 lg:row-start-1 flex flex-col gap-2">
             <span className="text-gray-1000 text-lg">تلفن پشتیبانی:‌</span>
             <span className="text-gray-700">021-8091607</span>
           </div>
-          <div className="col-span-4 md:col-span-2 flex row-start-4 md:row-start-2 lg:row-start-1 flex-col gap-2">
+          <div className="col-span-4 h-fit md:col-span-2 flex row-start-4 md:row-start-2 lg:row-start-1 flex-col gap-2">
             <span className="text-gray-1000 text-lg">آدرس پستی:</span>
             <p className="break-words text-gray-700">
               آدرس پستی: تهران. یوسف آباد. بین خیابان ۶۳ و ۶۵. پلاک ۴۸۵. واحد ۹۲
