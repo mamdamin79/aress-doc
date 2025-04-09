62c7e50b8f5bb240174d58967566513e152863cd
sinapir2 <sinapir2@gmail.com>
2025-04-09 14:31:06 +0330

    fix: remove unnecessary classes inside layout

--- apps/fe-app/app/layout.tsx
+++ apps/fe-app/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn('sdfsdf', 'dfsdfs')}>
+      <body>
         <Header />
         {children}
         <FooterLite />
