cfcc8caf8003ee1481d3d5912732f618b99ebf49
sinapir2 <sinapir2@gmail.com>
2025-04-16 16:00:35 +0330

    refactor: remove unrelated files

--- libs/design-system/src/lib/components/OptionsDropdown/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './OptionsDropdown';
-export * from './OptionsDropdownOption';
--- libs/design-system/src/lib/components/index.ts
+++ libs/design-system/src/lib/components/index.ts
@@ -17,7 +17,6 @@ export * from './Button';
 export * from './BulletList';
 export * from './DualSwitch';
 export * from './NotesHeading';
-export * from './OptionsDropdown';
 export * from './PopupInfo';
 export * from './Dialog';
 export * from './SquaredButton';
