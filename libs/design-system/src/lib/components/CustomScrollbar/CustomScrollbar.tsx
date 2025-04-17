4a278235ccdb98e4f9ff37b4a60973095be8748e
sinapir2 <sinapir2@gmail.com>
2025-04-17 12:29:22 +0330

    fix: remove custom scrollbar

--- libs/design-system/src/lib/components/CustomScrollbar/CustomScrollbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const CustomScrollbar: React.FC = () => {
-  return (
-    <div className="scrollbar scrollbar-thumb-red-700 scrollbar-thumb-rounded-full scrollbar-track-red-300 h-32 overflow-y-scroll">
-      <div className="h-64 bg-slate-400"></div>
-    </div>
-  );
-};
