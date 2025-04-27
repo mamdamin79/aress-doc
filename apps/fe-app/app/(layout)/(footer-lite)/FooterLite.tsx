00a141e1ec9c299f6014b08f1d478fc52882c87d
sina <sinapir2@gmail.com>
2025-04-27 14:26:05 +0330

    fix: change profile page file name

--- apps/fe-app/app/(layout)/(footer-lite)/FooterLite.tsx
+++ apps/fe-app/app/(layout)/(footer-lite)/FooterLite.tsx
@@ -1,4 +1,4 @@
-import Logo from '@aress-assets/icons/Logo.svg';
+import Logo from '@aress-assets/icons/logo.svg';
 import AbstractLine from '@aress-assets/icons/Abstract.svg';
 import Instagram from '@aress-assets/icons/lite_instagram.svg';
 import Linkedin from '@aress-assets/icons/lite-linkedin.svg';
