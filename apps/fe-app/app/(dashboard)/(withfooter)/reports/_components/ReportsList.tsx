60890ffef2ac63602f92c93b42ea5aaf10d93aa8
mamdamin79 <mohammadaminsaheb@gmail.com>
2025-05-07 14:17:49 +0330

    fix: bugs in report list

--- apps/fe-app/app/(dashboard)/(withfooter)/reports/_components/ReportsList.tsx
+++ apps/fe-app/app/(dashboard)/(withfooter)/reports/_components/ReportsList.tsx
@@ -11,36 +11,45 @@ type Props = {
 };
 
 export const ReportList: React.FC<Props> = ({ reports }) => {
-  if (reports.length === 0) {
-    return (
-      <div className="flex items-center justify-center">
-        <div className="text-center">
-          <Image width={500} height={500} src={emptyState} alt="" />
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
-      <div className="flex items-center justify-center">
-        <div className="flex w-[380px] items-center gap-2">
-          <SearchBar />
-          <FilterReport />
+      <div className="inline-flex items-center justify-center">
+        <div className="flex w-full items-start">
+          <div className="flex items-center gap-2">
+            <SearchBar />
+            <FilterReport />
+          </div>
         </div>
       </div>
-      <div className="3xl:grid-cols-3 grid grid-cols-1 items-center gap-4 xl:grid-cols-2">
-        {reports.map((report, idx) => (
-          <ReportCard
-            link="/report/1"
-            categoryType={report.category.title}
-            reportSubscription="رایگان"
-            fixedBrief={true}
-            key={report.identifier}
-            {...report}
+      {reports.length > 0 ? (
+        <div className="4xl:grid-cols-3 4xl:max-w-[1591px] grid max-w-[1048px] grid-cols-1 items-center gap-4 xl:grid-cols-2">
+          {reports.map((report, idx) => (
+            <div
+              key={report.identifier}
+              className="sm:max-w-[380px] md:max-w-full"
+            >
+              <ReportCard
+                link="/report/1"
+                categoryType={report.category.title}
+                reportSubscription="رایگان"
+                fixedBrief={true}
+                {...report}
+              />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="4xl:w-[1591px] mx-auto flex h-full flex-col items-center justify-start md:w-[512px] xl:w-[904px]">
+          <Image
+            src={emptyState}
+            alt="empty state"
+            className="h-[345px] w-[380px] object-contain md:h-[380px] md:w-[412px]"
           />
-        ))}
-      </div>
+          <p className="text-lg font-normal text-gray-500">
+            گزارشی برای نمایش وجود ندارد
+          </p>
+        </div>
+      )}
     </>
   );
 };
