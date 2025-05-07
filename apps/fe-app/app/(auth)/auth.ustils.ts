0fd72abf05d1d7dc9b932cf12c4e98bca08b3bd4
mamdamin79 <mohammadaminsaheb@gmail.com>
2025-05-07 14:22:52 +0330

    feat: add function that request and get token

---
+++ apps/fe-app/app/(auth)/auth.ustils.ts
@@ -0,0 +1,22 @@
+export async function fetchToken(): Promise<string> {
+    const response = await fetch('http://185.141.213.190:8000/users/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        grant_type: 'password',
+        username: 'test',
+        password: 'test',
+      }).toString(),
+    });
+  
+    if (!response.ok) {
+      throw new Error('Failed to fetch access token');
+    }
+  
+    const data = await response.json();
+
+    return data.access_token;
+  }
+  
