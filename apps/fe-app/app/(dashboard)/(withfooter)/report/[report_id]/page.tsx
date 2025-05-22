cf90852c01637f2625823166bd9b3ad610ecf2c4
sinapir2 <sinapir2@gmail.com>
2025-05-22 13:51:39 +0330

    fix: fix report overview page

--- apps/fe-app/app/(dashboard)/(withfooter)/report/[report_id]/page.tsx
+++ apps/fe-app/app/(dashboard)/(withfooter)/report/[report_id]/page.tsx
@@ -21,17 +21,8 @@ async function getData(id: number) {
   })) as ReportDetailPageApiResponse;
   return user;
 }
-function cleanServerMarkdown(raw: string): string {
-  return raw
-    .replace(/\\n/g, '\n') // Turn escaped \n into real newlines
-    .split('\n') // Split by lines
-    .map((line) => line.trimStart()) // Remove leading spaces (prevents code blocks)
-    .join('\n') // Join back
-    .trim(); // Remove extra whitespace at start/end
-}
 const page = async () => {
   const REPORT = await getData(1);
-  const cleanMarkdown = cleanServerMarkdown(REPORT?.markdownDescription);
   const baseURL = process.env.NEXT_PUBLIC_API_URL;
   return (
     <div className="mx-auto max-w-[1680px]">
@@ -81,53 +72,56 @@ const page = async () => {
         id="1"
       >
         <SectionTitle align="center" level={3} title="اطلاعات بیشتر" />
-        <div className="mt-12 flex flex-col text-right">
+        <div className="mt-2 flex flex-col text-right">
           {/* Section Component */}
           <Markdown
             components={{
               // Custom renderer for the ul element
               ul: ({ node, ...props }) => (
                 <ul
-                  className="rtl marker:text-brand-600 list-disc text-right text-xl font-medium marker:text-3xl"
+                  className="rtl marker:text-brand-600 list-disc text-right marker:text-3xl"
                   {...props}
                 />
               ),
               // Custom renderer for h1 (titles)
               h1: ({ node, ...props }) => (
-                <h1
-                  className="mt-10 text-right text-xl font-medium"
-                  {...props}
-                />
+                <div className="before:bg-brand-600 relative pr-4 before:absolute before:bottom-3.5 before:right-0 before:h-2 before:w-2 before:rounded-full before:content-['']">
+                  <h1
+                    className="pt-10 text-right text-xl font-medium"
+                    {...props}
+                  />
+                </div>
               ),
               // Custom renderer for h2 (subtitles)
 
               h2: ({ node, ...props }) => (
-                <h2
-                  className="mb-4 text-right text-lg font-medium"
-                  {...props}
-                />
+                <h2 className="text-right text-lg font-medium" {...props} />
               ),
               // Custom renderer for paragraphs
               p: ({ node, ...props }) => (
                 <p
-                  className="text-md mb-4 text-right leading-relaxed text-gray-600"
+                  className="text-md mt-4 text-right font-normal leading-relaxed text-gray-600"
                   {...props}
                 />
+              ),
+              // ** strong **
+              strong: ({ node, ...props }) => (
+                <strong className="text-lg font-medium text-black" {...props} />
               ),
               // Custom renderer for ordered lists
               ol: ({ node, ...props }) => (
                 <ol
-                  className="list-decimal pr-2.5 text-right leading-relaxed"
+                  className="list-decimal pr-2.5 text-right text-lg font-medium leading-relaxed"
                   {...props}
                 />
               ),
               // Custom renderer for list items
               li: ({ node, ...props }) => (
-                <li className="mb-4 text-right" {...props} />
+                <li className="text-right" {...props} />
               ),
             }}
           >
-            {cleanMarkdown}
+            {REPORT?.markdownDescription}
           </Markdown>
         </div>
       </section>
