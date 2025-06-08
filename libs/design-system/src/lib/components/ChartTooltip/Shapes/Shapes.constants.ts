85db2bc48273eb4045dd52a541b9860b2df10255
sinapir2 <sinapir2@gmail.com>
2025-06-08 14:29:37 +0330

    fix: remove chart tooltip

--- libs/design-system/src/lib/components/ChartTooltip/Shapes/Shapes.constants.ts
+++
@@ -1,9 +0,0 @@
-export const bgClassMap = {
-  purple: 'text-surface-accent-purple-600',
-  blue: 'text-surface-accent-blue-600',
-  yellow: 'text-surface-accent-yellow-600',
-  red: 'text-surface-accent-red-600',
-};
-
-export const strokeClass = 'stroke-border-neutral-oninverse';
-export const strokeWidth = 8;
