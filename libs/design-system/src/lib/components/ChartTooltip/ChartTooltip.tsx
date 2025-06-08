80b084428408399390a1c560b91a80b0ff6fe91a
sinapir2 <sinapir2@gmail.com>
2025-06-08 14:28:00 +0330

    feat: create report 6

--- libs/design-system/src/lib/components/ChartTooltip/ChartTooltip.tsx
+++ libs/design-system/src/lib/components/ChartTooltip/ChartTooltip.tsx
@@ -1,39 +1,26 @@
 'use client';
 import { cn } from '../../../utils/classNames.utils';
-import React, { useId, cloneElement, ReactElement } from 'react';
-import { Tooltip as ReactTooltip } from 'react-tooltip';
-import 'react-tooltip/dist/react-tooltip.css';
+import React, { ReactElement } from 'react';
 import { TooltipRow, TooltipRowProps } from './TooltipRow';
 
 type Props = {
   children: ReactElement;
   title: string;
   items: TooltipRowProps[];
-  position?: 'top' | 'right' | 'bottom' | 'left';
   className?: string;
-  offset?: number;
 };
 
 export const ChartTooltip: React.FC<Props> = ({
   children,
   className,
-  position = 'top',
-  offset = 4,
   title,
   items,
 }) => {
-  const id = useId();
-
   return (
     <>
-      {cloneElement(children, {
-        'data-tooltip-id': id,
-      })}
+      {children}
 
-      <ReactTooltip
-        id={id}
-        place={position}
-        offset={offset}
+      <div
         style={{ padding: '8px 16px' }}
         className={cn(
           'bg-surface-neutral-invers text-text-neutral-oninverse z-50 flex flex-col gap-1 rounded-lg text-sm font-medium',
@@ -44,9 +31,7 @@ export const ChartTooltip: React.FC<Props> = ({
         {items.map((item, index) => (
           <TooltipRow key={`tooltip-row-${index}`} {...item} />
         ))}
-      </ReactTooltip>
+      </div>
     </>
   );
 };
-
-export default ChartTooltip;
---
+++ libs/design-system/src/lib/components/Reports/Report.config.shared.ts
@@ -0,0 +1,103 @@
+export const baseOptions: Highcharts.Options = {
+  chart: {
+    backgroundColor: 'var(--color-surface-neutral-primary)',
+    style: {
+      fontFamily: 'Vazirmatn',
+      width: 616,
+      height: 286,
+    },
+  },
+  legend: {
+    align: 'right',
+    verticalAlign: 'top',
+    layout: 'horizontal',
+    rtl: true,
+    itemStyle: {
+      color: 'var(--color-text-neutral-primary)', // Or any custom color
+      fontSize: '12px',
+      fontWeight: '500',
+    },
+  },
+  title: {
+    text: '',
+  },
+  credits: {
+    enabled: false,
+  },
+  tooltip: {
+    shared: true,
+    useHTML: true,
+    backgroundColor: 'transparent', // required for custom styling
+    borderWidth: 0, // remove default border
+    shadow: false, // remove default shadow
+    style: {
+      direction: 'rtl',
+    },
+    headerFormat: `
+            <div dir="rtl"
+              style="
+                background: var(--color-surface-neutral-inverse);
+                color: var(--color-text-neutral-oninverse);
+                border-radius: 10px;
+                padding: 8px 16px;
+                font-family: Vazirmatn;
+                font-size: 14px;
+                font-weight: 500;
+                line-height: 26px;
+                text-align: right;
+                box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+                margin-bottom: 4px;
+                backdrop-filter: blur(6px);
+              -webkit-backdrop-filter: blur(6px);
+              "
+            >
+              <div style="font-weight: 500;">{point.key}</div>
+          `,
+    pointFormat: `
+          <div style="display: flex; align-items: center; gap: 4px;">
+            <span style="color:{series.color}; font-size: 12px;">●</span>
+            <div style="display: flex; align-items: center; gap: 4px; justify-content: space-between; width: 100%;">
+              <span style="font-size: 14px; font-weight: 400;">{series.name}:</span>
+<span style="direction: ltr; unicode-bidi: plaintext; display: inline-block; min-width: 40px;">{point.y}</span>
+            </div>
+          </div>
+        `,
+    footerFormat: `</div>`,
+  },
+  plotOptions: {
+    column: {
+      grouping: false,
+      borderWidth: 0,
+      borderRadius: 4,
+    },
+    spline: {
+      marker: {
+        enabled: false,
+      },
+    },
+  },
+};
+export const xAxisLabels: Highcharts.XAxisLabelsOptions = {
+  style: {
+    fontSize: '12px',
+    fontWeight: '400',
+    color: 'var(--color-text-neutral-primary)',
+  },
+  formatter: function (this: Highcharts.AxisLabelsFormatterContextObject) {
+    const chart = this.axis.chart;
+    const totalLabels = this.axis.categories?.length ?? 12;
+    const spacePerLabel = chart.plotWidth / totalLabels;
+
+    const maxChars = spacePerLabel < 50 ? 3 : 6;
+
+    const label = this.value as string;
+    return label.length > maxChars ? label.slice(0, maxChars) : label;
+  },
+};
+export const yAxisLabels: Highcharts.YAxisLabelsOptions = {
+  style: {
+    fontSize: '12px',
+    fontWeight: '400',
+    color: 'var(--color-text-neutral-secondarycontrast)',
+  },
+};
