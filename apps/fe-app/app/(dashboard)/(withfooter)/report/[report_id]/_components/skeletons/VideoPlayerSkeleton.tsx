2e76ad23b1e56b30457161e7085f6ba221a3f7aa
Reza <rezasiahi8@gmail.com>
2025-07-15 10:36:02 +0330

    feat: add skeleton for single report page

---
+++ apps/fe-app/app/(dashboard)/(withfooter)/report/[report_id]/_components/skeletons/VideoPlayerSkeleton.tsx
@@ -0,0 +1,5 @@
+export function VideoPlayerSkeleton () {
+    return (
+        <div className="w-[818px] h-[450px] rounded-lg skeleton-shimmer" />
+    )
+}
--- apps/fe-app/app/(dashboard)/(withfooter)/report/[report_id]/page.tsx
+++ apps/fe-app/app/(dashboard)/(withfooter)/report/[report_id]/page.tsx
@@ -10,6 +10,13 @@ import leftWaveSVG from '@aress-assets/images/leftwaves.svg';
 import Image from 'next/image';
 import { OpenAPI, ReportsService } from '@openapi';
 import { ReportWrapper } from './_components/ReportWrapper';
+import { ReportSectionSkeleton } from '../../../(nofooter)/(dashboard)/_components/skeletons/ReportSectionSkeleton';
+import { ReportTitleSectinoSkeleton } from './_components/skeletons/ReportTitleSectionSkeleton';
+import { ReportTitleSettingSkeleton } from './_components/skeletons/ReportTitleSettingSkeleton';
+import { ReportTabSkeleton } from './_components/skeletons/ReportTabSkeleton';
+import { VideoPlayerSkeleton } from './_components/skeletons/VideoPlayerSkeleton';
+import { MarkdownSkeleton } from './_components/skeletons/MarkdownSkeleton';
+import { RelationReportSkeleton } from './_components/skeletons/RelationReportSkeleton';
 async function getData(id: number) {
   const token = await fetchToken();
   if (!token) {
@@ -26,6 +33,9 @@ async function getData(id: number) {
 const page = async () => {
   const REPORT = await getData(6);
   const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+  console.log(REPORT.relatedReports);
+
   return (
     <>
       <div className="px-8 pt-3">
@@ -43,13 +53,11 @@ const page = async () => {
 
         <section className="mb-16 flex w-full flex-col-reverse items-center gap-8 px-20 pt-6 xl:flex-row xl:items-start xl:justify-around">
           <div className="flex w-fit flex-col gap-6">
-            <ReportWrapper
-              data={REPORT.reportCalculation}
-              title={REPORT.title}
-              identifier={REPORT.identifier}
-            />
+            {
+              <ReportSectionSkeleton />
+            }
 
-            <div className="flex w-fit flex-row items-center gap-1 text-sm font-normal">
+            {/* <div className="flex w-fit flex-row items-center gap-1 text-sm font-normal">
               <Icon name="info" size="md" />
               <span>با زدن بر روی آیکون </span>
               <span className="flex flex-row items-center">
@@ -57,21 +65,29 @@ const page = async () => {
                 {')'}
               </span>
               <span> امکان تغییر تنظیمات پیشفرض پروژه وجود دارد.</span>
-            </div>
+            </div> */}
+            <ReportTitleSettingSkeleton />
           </div>
-          <ReportOverview
+          {/* <ReportOverview
             title={REPORT?.title}
             category={REPORT?.category}
             summary={REPORT?.summary}
             userFavorite={REPORT?.userFavorite}
             isNew={REPORT?.isNew}
-          />
+          /> */}
+          <ReportTitleSectinoSkeleton />
         </section>
-        <TabsWrapper />
+        {/* <TabsWrapper /> */}
+        <ReportTabSkeleton />
         <section className="flex w-full flex-col items-center px-20" id="0">
           <SectionTitle align="center" level={3} title="ویدیو بررسی" />
           <div className="mt-10">
-            <VideoPlayerWrapper data={REPORT.video} />
+            {
+              // REPORT.video ? 
+              // <VideoPlayerWrapper data={REPORT.video} />
+              // : 
+              <VideoPlayerSkeleton />
+            }
           </div>
         </section>
 
@@ -89,15 +105,20 @@ const page = async () => {
             <Image src={leftWaveSVG} alt="wave-left" fill />
           </div>
           <SectionTitle align="center" level={3} title="اطلاعات بیشتر" />
-          <div className="mt-2 flex flex-col text-right">
+          <div className="mt-2 flex flex-col text-right w-full">
             {/* Section Component */}
-            <MarkdownRender markdown={REPORT?.markdownDescription ?? ''} />
+            <div className='flex flex-col gap-[100px]'>
+              <MarkdownSkeleton />
+              <MarkdownSkeleton />
+              <MarkdownSkeleton />
+            </div>
+            {/* <MarkdownRender markdown={REPORT?.markdownDescription ?? ''} /> */}
           </div>
         </section>
 
         <section className="flex flex-col gap-12 pb-20 pt-[112px]" id="2">
           <SectionTitle align="center" level={2} title="گزارش‌های مرتبط" />
-          {REPORT?.relatedReports && (
+          {/* {REPORT?.relatedReports && (
             <ReportsCarousel
               cards={REPORT?.relatedReports.map((REPORT) => {
                 return {
@@ -112,7 +133,12 @@ const page = async () => {
                 };
               })}
             />
-          )}
+          )} */}
+          <div className='flex items-center justify-between'>
+            <RelationReportSkeleton />
+            <RelationReportSkeleton />
+            <RelationReportSkeleton />
+          </div>
         </section>
       </div>
     </>
