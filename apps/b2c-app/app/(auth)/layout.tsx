36ad22fff44a0341f92bec18172e4ffd7d3bf2ed
sinapir2 <sinapir2@gmail.com>
2025-07-22 14:34:05 +0330

    feat: create layout file

---
+++ apps/b2c-app/app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { IconProps, SquaredButton } from 'design-system';
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return { children };
+}
