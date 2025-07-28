a0443ba567d4ac1b8bb344bfbc6ce427307578a3
reza <rezasiahi8@gmail.com>
2025-07-28 10:47:38 +0330

    feat: create tradable badge component

--- libs/design-system/src/lib/components/index.ts
+++ libs/design-system/src/lib/components/index.ts
@@ -49,4 +49,5 @@ export * from './SparkLine';
 export * from './SummaryCell';
 export * from './TextField';
 export * from './DatePicker';
-export * from './Bookmark'
+export * from './Bookmark';
+export * from './TradableBadge';
