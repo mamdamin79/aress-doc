f135ace365775ba15e3ad0c56ad9fae4766507b1
reza <rezasiahi8@gmail.com>
2025-07-30 17:24:47 +0330

    feat: update summerycell component

--- libs/design-system/src/lib/components/SummaryCell/SummaryCell.tsx
+++ libs/design-system/src/lib/components/SummaryCell/SummaryCell.tsx
@@ -12,19 +12,18 @@ interface Props {
 
 export function SummaryCell({ label, value, subTitle }: Props) {
   return (
-    <div className="hover:shadow-3xl bg-surface-neutral-secondary group relative w-60 cursor-pointer overflow-hidden rounded-3xl p-6 duration-500">
-      <div className="bg-surface-neutral-tertiary absolute left-28 top-5 h-80 w-80 scale-0 rounded-full duration-500 group-hover:scale-[3]"></div>
-
+    <div className="hover:bg-surface-neutral-tertiary border-border-neutral-secondary bg-surface-neutral-primary group w-60 cursor-pointer overflow-hidden rounded-3xl border p-6">
       <div className="relative z-20 flex items-center gap-2">
-        <div className="group-hover:text-text-brand-primary-600 duration-300">
+        <div className="group-hover:text-text-brand-highcontrast-800 duration-300">
           <Icon size="lg" name={label.icon} />
         </div>
         <span className="text-sm font-medium group-hover:font-semibold">
           {label.title}
         </span>
       </div>
+      <div className="bg-border-neutral-tertiary my-2 h-[1px] w-full" />
       <div className="relative z-20 mt-5 flex flex-col items-center">
-        <span className="text-text-neutral-primary text-lg font-semibold group-hover:font-bold">
+        <span className="text-text-brand-highcontrast-800 text-lg font-semibold group-hover:font-bold">
           {value}
         </span>
         <span className="text-text-neutral-secondarycontrast text-sm group-hover:font-medium">
