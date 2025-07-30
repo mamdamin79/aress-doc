55bd278570c1916fc7527fc1c752dfeef71875d1
reza <rezasiahi8@gmail.com>
2025-07-30 16:45:27 +0330

    feat: update accordion component for use b2d app

--- libs/design-system/src/lib/components/Accordion/Accordion.tsx
+++ libs/design-system/src/lib/components/Accordion/Accordion.tsx
@@ -7,13 +7,21 @@ import {
 } from '@headlessui/react';
 import { Icon } from '../Icon';
 import { AccordionItem } from './Accordion.types';
+import { cn } from './../../../utils';
 
 type Props = {
   items: AccordionItem[];
   singleOpen: boolean;
+  mode: 'b2b' | 'b2c';
+  className?: string;
 };
 
-export const Accordion: React.FC<Props> = ({ items, singleOpen }) => {
+export const Accordion: React.FC<Props> = ({
+  items,
+  singleOpen,
+  mode,
+  className,
+}) => {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   return items.map((item, index) => (
@@ -21,32 +29,75 @@ export const Accordion: React.FC<Props> = ({ items, singleOpen }) => {
       as="div"
       key={item.title}
       onClick={() => setOpenIndex((prev) => (prev === index ? null : index))}
-      className="border-border-neutral-primary mb-2 w-full rounded-2xl border p-6"
+      className={cn(
+        'mb-2 w-full',
+        {
+          'border-border-neutral-primary rounded-2xl border p-6':
+            mode === 'b2b',
+          'py-4"': mode === 'b2c',
+        },
+        className,
+      )}
     >
       {({ open }) => (
         <>
           {singleOpen && open ? (
-            <button className="flex w-full items-center justify-between">
-              <span className="text-text-neutral-primary text-md font-semibold">
+            <button
+              className={cn('group flex w-full items-center justify-between', {
+                'py-4': mode === 'b2c',
+              })}
+            >
+              <span
+                className={cn(
+                  'text-text-neutral-primary text-md font-semibold',
+                  {
+                    'group-hover:text-icon-brand-primary-600': mode === 'b2c',
+                  },
+                )}
+              >
                 {item.title}
               </span>
               <span
-                className={`bg-button-brand-surface-default text-button-brand-label-onsurface flex h-8 w-8 items-center justify-center rounded-full transition-transform duration-300 ${
-                  open && openIndex === index ? 'rotate-180' : 'rotate-0'
-                }`}
+                className={cn(
+                  'flex items-center justify-center transition-transform duration-300',
+                  {
+                    'text-button-brand-label-onsurface bg-button-brand-surface-default h-8 w-8 rounded-full':
+                      mode === 'b2b',
+                    'text-icon-neutral-primary group-hover:text-icon-brand-primary-600':
+                      mode === 'b2c',
+                    'rotate-0': !open && openIndex === index,
+                    'rotate-180': open && openIndex !== index,
+                  },
+                )}
               >
                 <Icon name="chevron-down" size="lg" />
               </span>
             </button>
           ) : (
             <DisclosureButton className="flex w-full items-center justify-between">
-              <span className="text-text-neutral-primary text-md font-semibold">
+              <span
+                className={cn(
+                  'group flex w-full items-center justify-between',
+                  {
+                    'group-hover:text-text-brand-primary-600 py-4':
+                      mode === 'b2c',
+                  },
+                )}
+              >
                 {item.title}
               </span>
               <span
-                className={`bg-button-brand-surface-default text-button-brand-label-onsurface flex h-8 w-8 items-center justify-center rounded-full transition-transform duration-300 ${
-                  open ? 'rotate-180' : 'rotate-0'
-                }`}
+                className={cn(
+                  'flex items-center justify-center transition-transform duration-300',
+                  {
+                    'text-button-brand-label-onsurface bg-button-brand-surface-default h-8 w-8 rounded-full':
+                      mode === 'b2b',
+                    'text-icon-neutral-primary group-hover:text-icon-brand-primary-600':
+                      mode === 'b2c',
+                    'rotate-0': !open,
+                    'rotate-180': open,
+                  },
+                )}
               >
                 <Icon name="chevron-down" size="lg" />
               </span>
@@ -56,7 +107,14 @@ export const Accordion: React.FC<Props> = ({ items, singleOpen }) => {
             openIndex === index && (
               <DisclosurePanel
                 transition
-                className="border-border-neutral-primary text-text-neutral-secondary mt-4 border-t pt-4 text-sm duration-300 data-[closed]:-translate-y-6 data-[closed]:opacity-0"
+                className={cn(
+                  'text-sm duration-300 data-[closed]:-translate-y-6 data-[closed]:opacity-0',
+                  {
+                    'border-border-neutral-primary text-text-neutral-secondary mt-4 border-t pt-4':
+                      mode === 'b2b',
+                    'text-text-neutral-secondarycontrast pt-1': mode === 'b2c',
+                  },
+                )}
               >
                 {item.content}
               </DisclosurePanel>
@@ -64,7 +122,14 @@ export const Accordion: React.FC<Props> = ({ items, singleOpen }) => {
           ) : (
             <DisclosurePanel
               transition
-              className="border-border-neutral-primary text-text-neutral-secondary mt-4 border-t pt-4 text-sm duration-300 data-[closed]:-translate-y-6 data-[closed]:opacity-0"
+              className={cn(
+                'text-sm duration-300 data-[closed]:-translate-y-6 data-[closed]:opacity-0',
+                {
+                  'border-border-neutral-primary text-text-neutral-secondary mt-4 border-t pt-4':
+                    mode === 'b2b',
+                  'text-text-neutral-secondarycontrast pt-1': mode === 'b2c',
+                },
+              )}
             >
               {item.content}
             </DisclosurePanel>
