ea19ed7d727c2b9c8bc8c241231c313c843c9579
reza <rezasiahi8@gmail.com>
2025-07-30 17:56:35 +0330

    fix: change size summerycell component

--- libs/design-system/src/lib/components/SummaryCell/SummaryCell.tsx
+++ libs/design-system/src/lib/components/SummaryCell/SummaryCell.tsx
@@ -7,28 +7,22 @@ interface Props {
     title: string;
   };
   value: string;
-  subTitle?: string;
 }
 
-export function SummaryCell({ label, value, subTitle }: Props) {
+export function SummaryCell({ label, value }: Props) {
   return (
-    <div className="hover:bg-surface-neutral-tertiary border-border-neutral-secondary bg-surface-neutral-primary group w-60 cursor-pointer overflow-hidden rounded-3xl border p-6">
-      <div className="relative z-20 flex items-center gap-2">
-        <div className="group-hover:text-text-brand-highcontrast-800 duration-300">
-          <Icon size="lg" name={label.icon} />
+    <div className="hover:bg-surface-neutral-tertiary border-border-neutral-secondary bg-surface-neutral-primary group flex h-[132px] w-52 cursor-pointer items-center justify-center overflow-hidden rounded-3xl border p-5">
+      <div className="w-full">
+        <div className="relative z-20 flex items-center gap-2">
+          <div className="group-hover:text-text-brand-highcontrast-800 duration-300">
+            <Icon size="lg" name={label.icon} />
+          </div>
+          <span className="text-sm font-medium">{label.title}</span>
         </div>
-        <span className="text-sm font-medium group-hover:font-semibold">
-          {label.title}
-        </span>
-      </div>
-      <div className="bg-border-neutral-tertiary my-2 h-[1px] w-full" />
-      <div className="relative z-20 mt-5 flex flex-col items-center">
-        <span className="text-text-brand-highcontrast-800 text-lg font-semibold group-hover:font-bold">
+        <div className="bg-border-neutral-tertiary my-2 h-[1px] w-full" />
+        <div className="text-text-brand-highcontrast-800 text-center text-lg font-semibold">
           {value}
-        </span>
-        <span className="text-text-neutral-secondarycontrast text-sm group-hover:font-medium">
-          {subTitle}
-        </span>
+        </div>
       </div>
     </div>
   );
