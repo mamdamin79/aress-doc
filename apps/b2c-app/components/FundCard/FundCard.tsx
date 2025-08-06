4045f3178a88730298975be16e24e31fa3c93d7f
reza <rezasiahi8@gmail.com>
2025-08-06 09:21:24 +0330

    feat: set custom background for any category badge

--- apps/b2c-app/components/FundCard/FundCard.tsx
+++ apps/b2c-app/components/FundCard/FundCard.tsx
@@ -1,4 +1,4 @@
-import { Badge } from 'design-system';
+import { Badge, BadgeProps } from 'design-system';
 import React from 'react';
 interface FundProps {
   name: string;
@@ -7,17 +7,27 @@ interface FundProps {
 }
 
 export const FundCard: React.FC<FundProps> = ({ fundCategory, logo, name }) => {
+  const themeMap: Record<FundProps['fundCategory'], string> = {
+    سهامی: 'green',
+    'درآمد ثابت': 'blue',
+    مختلط: 'purple',
+    کالایی: 'yellow',
+  };
+
   return (
     <div className="border-border-neutral-primary flex w-full items-center justify-between rounded-2xl border px-4 py-2">
       <div className="flex items-center gap-2">
         <img
           src={logo ?? ''}
-          alt="fund logo"
+          alt={`${name} logo`}
           className="h-[38px] w-[38px] rounded-full"
         />
         <span className="text-sm font-medium">{name}</span>
       </div>
-      <Badge theme="green" title={fundCategory} />
+      <Badge
+        theme={themeMap[fundCategory] as BadgeProps['theme']}
+        title={fundCategory}
+      />
     </div>
   );
 };
