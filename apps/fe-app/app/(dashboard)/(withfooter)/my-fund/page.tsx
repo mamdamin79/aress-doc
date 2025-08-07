b1d0bfb7efbc966eb1612042eff893e709015697
mamdamin79 <mohammadaminsaheb@gmail.com>
2025-08-07 12:31:07 +0330

    feat: fix margins

--- apps/fe-app/app/(dashboard)/(withfooter)/my-fund/page.tsx
+++ apps/fe-app/app/(dashboard)/(withfooter)/my-fund/page.tsx
@@ -1,9 +1,9 @@
-import { Breadcrumb } from 'design-system';
+import { Breadcrumb, Button, FundsLogo, Tabs } from 'design-system';
 
 export default function MyFund() {
   return (
     <div>
-      <div className="mr-32">
+      <div className="mb-1 mr-8 mt-2">
         <Breadcrumb
           items={[
             {
@@ -15,6 +15,44 @@ export default function MyFund() {
             },
             {
               title: 'صندوق سرمایه گذاری سهم آشنا',
+            },
+          ]}
+        />
+      </div>
+      <div className="mt-7 px-20">
+        <div className="mb-7 flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <FundsLogo hasTag size="md" />
+            <div className="text-text-neutral-primary text-xl font-semibold">
+              صندوق سرمایه گذاری سهم آشنا
+              <span className="font-medium"> (در سهام)</span>
+            </div>
+          </div>
+          <Button
+            className="w-[183px]"
+            iconRight={{ name: 'plus', size: 'lg' }}
+          >
+            افزودن به دیده‌بان
+          </Button>
+        </div>
+        <Tabs
+          activeTab={0}
+          className="w-full"
+          colorMode="neutral"
+          // onClickTab={(id) => console.log(id)}
+          variant="lined"
+          tabs={[
+            {
+              id: 'tab1',
+              title: 'خلاصه',
+            },
+            {
+              id: 'tab2',
+              title: 'تحلیل بازدهی',
+            },
+            {
+              id: 'tab3',
+              title: 'ارزیابی ریسک',
             },
           ]}
         />
