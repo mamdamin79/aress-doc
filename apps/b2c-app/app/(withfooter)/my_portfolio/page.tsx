c1d7f52afab12ac4c02899255b32f216b08f477f
sinapir2 <sinapir2@gmail.com>
2025-08-09 10:42:19 +0330

    feat: scaffold my_portfolio page

---
+++ apps/b2c-app/app/(withfooter)/my_portfolio/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MyPortfolio = () => {
+  return (
+    <>
+      <div className="text-text-neutral-primary flex flex-col gap-1 px-20 pt-6">
+        <div className="flex flex-col gap-4 pb-6">
+          <h1 className="text-xl font-semibold">دارایی من</h1>
+          <div className="bg-surface-brand-200 h-[66px] w-full">
+            {/* tooltip for chart */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MyPortfolio;
