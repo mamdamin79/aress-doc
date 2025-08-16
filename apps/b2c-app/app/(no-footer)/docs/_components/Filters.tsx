6c58dd1db32d36c9c530c98bc48b0bfdad6f9be7
sinapir2 <sinapir2@gmail.com>
2025-08-16 12:37:39 +0330

    feat: extend accordion component

--- apps/b2c-app/app/(no-footer)/docs/_components/Filters.tsx
+++ apps/b2c-app/app/(no-footer)/docs/_components/Filters.tsx
@@ -1,5 +1,29 @@
+'use client';
+import { Button, Icon } from 'design-system';
 import React from 'react';
 
 export const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    <div className="flex flex-col items-end gap-[27px]">
+      <Button mode="secondary" className="w-[170px]" size="sm">
+        <div className="flex items-center gap-2 font-medium">
+          <span>درخواست گزارش</span>
+          <Icon name="file-plus-2" />
+        </div>
+      </Button>
+      <div className="flex w-full items-center justify-between">
+        <span className="text-text-neutral-secondary text-lg font-semibold">
+          فیلترها
+        </span>
+        <Button
+          mode="secondary"
+          size="sm"
+          theme="error"
+          className="w-[120px] font-medium"
+        >
+          حذف فیلترها
+        </Button>
+      </div>
+    </div>
+  );
 };
