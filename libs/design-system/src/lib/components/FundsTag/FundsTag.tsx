9fa0583053d889e151c5a0daa6c914b852b3188a
mamdamin79 <mohammadaminsaheb@gmail.com>
2025-08-17 17:53:23 +0330

    feat: some fixes - and improve fundstag to get size

--- libs/design-system/src/lib/components/FundsTag/FundsTag.tsx
+++ libs/design-system/src/lib/components/FundsTag/FundsTag.tsx
@@ -1,21 +1,32 @@
 import { cn } from '../../../utils/classNames.utils';
 import React from 'react';
+
 export interface FundsTagProps {
   color: 'purple' | 'blue' | 'green' | 'yellow' | 'pink' | 'neutral';
+  size?: 'lg' | 'md';
 }
-export const FundsTag: React.FC<FundsTagProps> = ({ color }) => {
+
+export const FundsTag: React.FC<FundsTagProps> = ({ color, size = 'md' }) => {
   return (
-    <div className="bg-surface-neutral-secondary flex h-3 w-3 items-center justify-center rounded-full">
+    <div
+      className={cn(
+        'bg-surface-neutral-secondary flex items-center justify-center rounded-full',
+        size === 'md' && 'h-3 w-3',
+        size === 'lg' && 'h-3.5 w-3.5',
+      )}
+    >
       <div
         className={cn(
-          'h-2.5 w-2.5 rounded-full',
+          'rounded-full',
+          size === 'md' && 'h-2.5 w-2.5',
+          size === 'lg' && 'h-3 w-3',
           color === 'green' && 'bg-surface-accent-vividgreen-600',
           color === 'blue' && 'bg-surface-accent-blue-600',
           color === 'purple' && 'bg-surface-accent-purple-600',
           color === 'yellow' && 'bg-surface-accent-yellow-600',
           color === 'pink' && 'bg-surface-accent-pink-600',
         )}
-      ></div>
+      />
     </div>
   );
 };
