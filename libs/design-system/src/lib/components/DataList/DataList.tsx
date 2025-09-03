6d3f975e9c9b5e0f1425bc2d8ccb2599888fb9fe
mamdamin79 <mohammadaminsaheb@gmail.com>
2025-09-03 13:31:19 +0330

    refactor: remove no need exports

--- libs/design-system/src/lib/components/DataList/DataList.tsx
+++ libs/design-system/src/lib/components/DataList/DataList.tsx
@@ -10,10 +10,6 @@ export type DataListProps = {
   mode?: 'vertical' | 'carousel';
 };
 
-export const GAP_WIDTH = 16;
-export const MAX_SLIDES = 4;
-export const CARD_WIDTH = 180;
-
 export const DataList: React.FC<DataListProps> = ({
   className,
   data,
